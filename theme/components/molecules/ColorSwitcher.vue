95f0064951d77983b3373df25dade9d6c0ae1982
Sébastien Chopin <seb@nuxtjs.com>
2021-02-16 18:10:31 +0100

    chore: add example and use fade animation for them switcher

--- theme/components/molecules/ColorSwitcher.vue
+++ theme/components/molecules/ColorSwitcher.vue
@@ -1,14 +1,25 @@
 <template>
   <span class="inline-flex">
     <button
-      class="text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 transition-colors duration-200 focus:outline-none"
+      class="text-gray-400 transition-colors duration-200 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 focus:outline-none"
       aria-label="Color Mode"
       @click="$colorMode.value === 'dark' ? $colorMode.preference = 'light' : $colorMode.preference = 'dark'"
     >
-      <client-only>
-        <IconSun v-if="$colorMode.value === 'light'" class="w-6 h-6" />
-        <IconMoon v-else class="w-6 h-6" />
-      </client-only>
+      <transition name="fade">
+        <client-only>
+          <IconSun v-if="$colorMode.value === 'light'" class="w-6 h-6" />
+          <IconMoon v-else class="w-6 h-6" />
+        </client-only>
+      </transition>
     </button>
   </span>
 </template>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .4s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
