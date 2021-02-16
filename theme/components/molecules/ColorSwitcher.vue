3c6ccd7909fbabc3fab2db643dedbd55dc5e4019
Sébastien Chopin <seb@nuxtjs.com>
2021-02-16 19:20:49 +0100

    chore: remove transition

--- theme/components/molecules/ColorSwitcher.vue
+++ theme/components/molecules/ColorSwitcher.vue
@@ -5,34 +5,10 @@
       aria-label="Color Mode"
       @click="$colorMode.value === 'dark' ? $colorMode.preference = 'light' : $colorMode.preference = 'dark'"
     >
-      <transition :name="mounted ? 'none' : 'fade'" mode="out-in">
-        <client-only>
-          <IconSun v-if="$colorMode.value === 'light'" class="w-6 h-6" />
-          <IconMoon v-else class="w-6 h-6" />
-        </client-only>
-      </transition>
+      <client-only>
+        <IconSun v-if="$colorMode.value === 'light'" class="w-6 h-6" />
+        <IconMoon v-else class="w-6 h-6" />
+      </client-only>
     </button>
   </span>
 </template>
-
-<script>
-export default {
-  data () {
-    return { mounted: false }
-  },
-  mounted () {
-    setTimeout(() => {
-      this.mounted = true
-    }, 400)
-  }
-}
-</script>
-
-<style scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .2s;
-}
-.fade-enter, .fade-leave-to {
-  opacity: 0;
-}
-</style>
