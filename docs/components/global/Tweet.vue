060361b134ed5981ab2c714f3ef6a322854df1af
Ahad Birang <farnabaz@gmail.com>
2021-02-17 20:37:27 +0330

    feat: twitter card (#80)
    * feat: twitter card
    
    * support quote tweet
    
    * typo
    
    * cleanup
    
    * cache nodes, prevent re-fetch on file change
    
    * docs
    
    * add props

---
+++ docs/components/global/Tweet.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="tweet">
+    <div class="author flex mb-4">
+      <a :href="profileUrl" target="_blank" rel="noopener noreferrer">
+        <img :src="avatar" class="w-6 h-6 rounded-full" :class="{'h-12 w-12': layout === 'tweet'}" />
+      </a>
+      <div class="ml-2 flex-1">
+        <a :href="profileUrl" target="_blank" rel="noopener noreferrer">
+          <span class="font-bold text-black dark:text-white" :class="{'block': layout === 'tweet'}">
+            {{ name }}
+          </span>
+          <span class="text-sm text-gray-400">@{{ username }}</span>
+        </a>
+        <template v-if="layout !== 'tweet'">
+          ·
+          <a :href="tweetUrl" target="_blank" rel="noopener noreferrer" class="text-sm hover:text-blue-600">
+            {{ $d(createdAt, "long") }}
+          </a>
+        </template>
+      </div>
+      <a v-if="layout === 'tweet'" :href="tweetUrl" target="_blank" rel="noopener noreferrer">
+        <IconTwitter class="text-blue-500" />
+      </a>
+    </div>
+    <div class="content">
+      <slot />
+    </div>
+    <div v-if="layout === 'tweet'" class="mt-2 flex">
+      <a :href="likeUrl" target="_blank" rel="noopener noreferrer" class="flex items-center hover:text-red-600">
+        <IconHeart class="mr-2" />
+        {{ heartCount }}
+      </a>
+      <a :href="tweetUrl" target="_blank" rel="noopener noreferrer" class="ml-4 hover:text-blue-600">
+        {{ $d(createdAt, "long") }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: { type: String, default: '' },
+    name: { type: String, default: '' },
+    username: { type: String, default: '' },
+    avatar: { type: String, default: '' },
+    heartCount: { type: String, default: '' },
+    createdAt: { type: Number, default: 0 },
+    layout: { type: String, default: 'tweet' }
+  },
+  computed: {
+    tweetUrl () {
+      return `https://twitter.com/${this.username}/status/${this.id}`
+    },
+    profileUrl () {
+      return `https://twitter.com/${this.username}`
+    },
+    likeUrl () {
+      return `https://twitter.com/intent/like?tweet_id=${this.id}`
+    }
+  }
+}
+</script>
+
+<style>
+.tweet {
+  @apply my-5 p-6 pb-3 border border-gray-300 rounded-md mx-auto;
+  @apply dark:border-gray-700;
+  width: calc(min(100%, 550px))!important;
+}
+.tweet.tweet-quote {
+  @apply p-3;
+}
+.tweet .emoji {
+  @apply w-5 h-5 m-0 inline-block;
+}
+.tweet .content p > a {
+  @apply text-blue-600;
+}
+
+.tweet .image-container-1 {
+  @apply mt-2 flex flex-wrap rounded-md overflow-hidden;
+}
+.tweet .image-container-1 .media-image {
+  @apply w-full object-cover;
+}
+
+.tweet .image-container-2 {
+  @apply mt-2 grid grid-cols-2 gap-1 rounded-md overflow-hidden;
+}
+.tweet .image-container-2 .media-image {
+  height: 300px;
+  @apply object-cover;
+}
+
+.tweet .image-container-3 {
+  @apply mt-2 grid grid-cols-2 gap-1 grid-rows-2 rounded-md overflow-hidden;
+}
+.tweet .image-container-3 .media-image {
+  height: 150px;
+  @apply object-cover;
+}
+.tweet .image-container-3 .media-image:nth-child(3n+2) {
+  height: 100%;
+  @apply row-span-2;
+}
+
+.tweet .image-container-4 {
+  @apply mt-2 grid grid-cols-2 gap-1 grid-rows-2 rounded-md overflow-hidden;
+}
+.tweet .image-container-4 .media-image {
+  height: 150px;
+  @apply object-cover;
+}
+</style>
---
+++ theme/utils/remark-tweet.js
@@ -0,0 +1,89 @@
+const { fetchTweetAst } = require('static-tweets')
+
+function createTweetNode (data, content, layout = 'tweet') {
+  return [
+    {
+      type: 'html',
+      value: `<Tweet class="tweet tweet-${layout}" layout="${layout}" id="${data.id}" avatar="${data.avatar.normal}" :created-at="${data.createdAt}" heart-count="${data.heartCount}" name="${data.name}" username="${data.username}">`
+    },
+    ...mapAST(content),
+    { type: 'html', value: '</Tweet>' }
+  ]
+}
+
+function mapAST (ast) {
+  return ast.flatMap((node) => {
+    if (typeof node === 'string') {
+      return { type: 'text', value: node }
+    }
+    if (node.tag === 'a') {
+      return { type: 'link', data: node.props, url: node.props.href, children: mapAST(node.nodes) }
+    }
+    if (node.tag === 'br') {
+      return { type: 'html', value: '<br />' }
+    }
+    if (node.tag === 'img') {
+      if (node.props.dataType === 'emoji-for-text') {
+        return { type: 'html', value: `<img src="${node.props.src}" alt="${node.props.alt}" class="emoji" width="28" height="28" />` }
+      }
+      if (node.props.dataType === 'media-image') {
+        const { props } = node
+        return {
+          type: 'html',
+          value: `<img src="${props.src}" alt="${props.alt}" class="media-image" width="${props.width}" height="${props.height}" />`
+        }
+      }
+    }
+    if (node.tag === 'div') {
+      return [
+        { type: 'html', value: `<div class="${node.props.dataType.replace(/ /, '-')}">` },
+        ...mapAST(node.nodes),
+        { type: 'html', value: '</div>' }
+      ]
+    }
+
+    if (node.tag === 'p') {
+      return { type: 'paragraph', children: mapAST(node.nodes) }
+    }
+
+    // qoute
+    if (node.tag === 'blockquote') {
+      const { data, nodes } = node.data.ast[0]
+      return createTweetNode(data, nodes, 'tweet tweet-quote')
+    }
+
+    return { type: 'text', value: '?' }
+  })
+}
+
+const tweetCache = {}
+
+module.exports = () => {
+  return async (tree) => {
+    const modified = tree.children.map(async (node, i) => {
+      if (node.type === 'html' && node.value && node.value.match(/^\s*<[t|T]weet\s+/)) {
+        const match = node.value.match(/id=['"](\d*)['"]/)
+        if (!match) {
+          // eslint-disable-next-line no-console
+          console.error('Invalid tweet id')
+          return { type: 'html', value: '<!-- Invalid tweet id -->' }
+        }
+        if (!tweetCache[match[1]]) {
+          try {
+            const tweet = await fetchTweetAst(match[1])
+            const { nodes, data } = tweet[0]
+            tweetCache[match[1]] = createTweetNode(data, nodes)
+          } catch (e) {
+          // eslint-disable-next-line no-console
+            console.error(`Cannot fetch tweet ${match[1]}. ${e.message}`)
+            return { type: 'html', value: `<!-- Cannot fetch tweet ${match[1]} -->` }
+          }
+        }
+        return tweetCache[match[1]]
+      }
+      return Promise.resolve(node)
+    })
+    tree.children = (await Promise.all(modified)).flat()
+    return null
+  }
+}
