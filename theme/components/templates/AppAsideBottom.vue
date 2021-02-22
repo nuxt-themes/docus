50245ea00b60daaac2c7c58635d8f376cdc81fb9
Farnabaz <farnabaz@gmail.com>
2021-02-22 13:18:25 +0330

    feat(slots): support customizable slots

---
+++ theme/components/templates/AppAsideBottom.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="showPlaceholder" class="flex items-center justify-center bg-gray-400 h-24">
+    AppAsideBottom
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showPlaceholder: this.$route.query.slots
+    }
+  }
+}
+</script>
