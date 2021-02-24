d0ff5c986e0061f5a0a906be0ba9ef8904122f77
Ahad Birang <farnabaz@gmail.com>
2021-02-25 02:00:34 +0330

    feat: detect title and description of document (#98)
    * feat: detect title and description of document
    
    * chore: update markdown files

--- theme/utils/document.js
+++ theme/utils/document.js
@@ -20,6 +20,50 @@ export function isDraft (path) {
   return !!path.match(/(\.draft)$/, '$2')
 }
 
+export function processDocumentInfo (document) {
+  if (document.title && document.description) {
+    return document
+  }
+  const [first, second] = document.body.children
+    .flatMap(node => node.children ? node.children : node)
+    // top level `text` can be ignored
+    .filter(node => node.type !== 'text')
+
+  if (first && first.tag === 'h1') {
+    if (!document.title) {
+      document.title = getTextContent(first)
+      Object.assign(first, {
+        type: 'text', value: ''
+      })
+    }
+    // look for second element to find description
+    if (second && second.tag === 'blockquote') {
+      if (!document.description) {
+        document.description = getTextContent(second)
+        Object.assign(second, {
+          type: 'text', value: ''
+        })
+      }
+    }
+  } else if (first && first.type === 'blockquote') {
+    if (!document.description) {
+      document.description = getTextContent(first)
+      Object.assign(first, {
+        type: 'text', value: ''
+      })
+    }
+  }
+  return document
+}
+
+function getTextContent (node) {
+  let text = node.value || ''
+  if (node.children) {
+    text = text + node.children.map(child => getTextContent(child)).join('')
+  }
+  return text
+}
+
 function paddLeft (value, length) {
   return ('0'.repeat(length) + value).substr(String(value).length)
 }
