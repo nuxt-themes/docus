3c68aa95266eb53afa1f392d46d7f4d64061b39a
Daniel Roe <daniel@roe.dev>
2021-02-27 15:19:28 +0000

    feat: `<inject-content>` component

---
+++ docs/content/2.usage/_example.md
@@ -0,0 +1,8 @@
+---
+draft: true
+---
+
+```js
+// This is some sample shared code.
+export defauit {}
+```
---
+++ theme/components/atoms/InjectContent.vue
@@ -0,0 +1,17 @@
+<template>
+  <nuxt-content :document="page" />
+</template>
+
+<script>
+export default {
+  props: {
+    query: { type: String, required: true }
+  },
+  data: () => ({
+    page: []
+  }),
+  async fetch () {
+    this.page = await this.$content(this.query).fetch()
+  }
+}
+</script>
