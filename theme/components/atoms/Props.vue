92e11e375b2d790decefe911a0bef7f5c4dee52a
Farnabaz <farnabaz@gmail.com>
2021-03-03 21:09:09 +0330

    move table-container

--- theme/components/atoms/Props.vue
+++ theme/components/atoms/Props.vue
@@ -1,35 +1,39 @@
 <template>
-  <table v-if="component">
-    <thead>
-      <tr>
-        <th>Prop</th>
-        <th>Type</th>
-        <th v-if="showRequired">Required</th>
-        <th v-if="showDefault">Default</th>
-        <th v-if="showValues">Values</th>
-        <th v-if="showDescription">Description</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="prop in props" :key="prop.name">
-        <td><code>{{ prop.name }}</code></td>
-        <td><code>{{ prop.type && prop.type.name }}</code></td>
-        <td v-if="showRequired">{{ prop.required ? "Yes" : "No" }}</td>
-        <td v-if="showDefault"><code v-if="prop.defaultValue">{{ prop.defaultValue && prop.defaultValue.value }}</code></td>
-        <td v-if="showValues">
-          <code v-if="prop.values">{{ prop.values && JSON.stringify(prop.values).replace(/,/g, ', ') }}</code>
-          <span v-else>-</span>
-        </td>
-        <td v-if="showDescription">
-          <div v-html="prop.description"></div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <table-container>
+    <table v-if="component">
+      <thead>
+        <tr>
+          <th>Prop</th>
+          <th>Type</th>
+          <th v-if="showRequired">Required</th>
+          <th v-if="showDefault">Default</th>
+          <th v-if="showValues">Values</th>
+          <th v-if="showDescription">Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="prop in props" :key="prop.name">
+          <td><code>{{ prop.name }}</code></td>
+          <td><code>{{ prop.type && prop.type.name }}</code></td>
+          <td v-if="showRequired">{{ prop.required ? "Yes" : "No" }}</td>
+          <td v-if="showDefault"><code v-if="prop.defaultValue">{{ prop.defaultValue && prop.defaultValue.value }}</code></td>
+          <td v-if="showValues">
+            <code v-if="prop.values">{{ prop.values && JSON.stringify(prop.values).replace(/,/g, ', ') }}</code>
+            <span v-else>-</span>
+          </td>
+          <td v-if="showDescription">
+            <div v-html="prop.description"></div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </table-container>
 </template>
 
 <script>
+import TableContainer from '../../../docs/components/TableContainer.vue'
 export default {
+  components: { TableContainer },
   props: {
     of: {
       type: String,
