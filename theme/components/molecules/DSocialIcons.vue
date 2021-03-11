118ecbc5e6eb8e1e4f33f6246bd5ca2c43c39b41
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2021-03-11 22:59:03 +0200

    fix: update header icons optical sizes (#156)

--- theme/components/molecules/DSocialIcons.vue
+++ theme/components/molecules/DSocialIcons.vue
@@ -9,7 +9,7 @@
       name="Twitter"
       class="text-gray-400 transition-colors duration-200 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400"
     >
-      <IconTwitter class="w-5 h-5" />
+      <IconTwitter class="w-6 h-6" />
     </a>
     <a
       v-if="settings.github.repo"
@@ -20,7 +20,7 @@
       name="Github"
       class="text-gray-400 transition-colors duration-200 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400"
     >
-      <IconGithub class="w-5 h-5" />
+      <IconGithub class="w-6 h-6" />
     </a>
   </div>
 </template>
