327d7e6d7d5a6b791045c637fb9f885bd8b3dc60
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2021-03-12 15:09:16 +0200

    fix: add category to prev/next; improve styling

--- theme/pages/_.vue
+++ theme/pages/_.vue
@@ -45,7 +45,7 @@ export default {
 
     const [prev, next] = await $content({ deep: true })
       .where({ language, draft, menu: { $ne: false } })
-      .only(['title', 'slug', 'to'])
+      .only(['title', 'slug', 'to', 'category'])
       .sortBy('position', 'asc')
       .surround(document.slug, { before: 1, after: 1 })
       .fetch()
