e6669f66da34ef5b47152a1b93e4f67fdb022d13
Sébastien Chopin <seb@nuxtjs.com>
2021-03-12 16:39:19 +0100

    chore: wip on HeaderRightCTA

---
+++ docs/components/HeaderRightCTA.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLink v-show="$route.path === '/'" to="/get-started/installation" class="inline-flex items-center flex-none px-3 py-2 text-base font-semibold leading-4 text-white transition-colors duration-200 border border-transparent rounded bg-primary-500 hover:bg-primary-600">
+    Get started
+  </NuxtLink>
+</template>
+
+<script>
+// TODO: $route.path === '/' becomes $page.slug === 'index' becomes $docus.isPage('index')
+</script>
