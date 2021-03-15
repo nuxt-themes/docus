4f1f5ef3fa8dda3cfd95b8a0b2b53d7633bb2d85
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2021-03-15 17:40:51 +0200

    fix: article padding for readme (#176)

--- theme/components/templates/AppPage.vue
+++ theme/components/templates/AppPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex w-full pt-10 ">
-    <article class="flex-auto min-w-0 px-4 mb-8 sm:px-6 xl:px-8" :class="prose ? 'prose dark:prose-dark' : ''"><slot /></article>
+    <article class="flex-auto min-w-0 px-4 mb-8 sm:px-6" :class="prose ? 'prose dark:prose-dark' : ''"><slot /></article>
     <slot name="toc" />
   </div>
 </template>
