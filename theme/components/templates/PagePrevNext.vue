1ef2595ad607077f56e314a4b03768581c23fe75
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2021-03-15 11:36:59 +0200

    fix: add category to prev/next; improve styling (#164)
    * fix: add category to prev/next; improve styling
    
    * improvements
    
    * missed class
    
    * fixes
    
    * more fixes for dark mode

--- theme/components/templates/PagePrevNext.vue
+++ theme/components/templates/PagePrevNext.vue
@@ -1,24 +1,36 @@
 <template>
-  <div v-if="prev || next" class="flex flex-col justify-between py-4 space-y-2 font-medium leading-7 xs:space-y-0 xs:space-x-3 xs:flex-row">
-    <NuxtLink
-      v-if="prev"
-      :to="$contentLocalePath(prev.to)"
-      class="flex items-center justify-start w-full px-4 py-4 truncate border border-gray-100 rounded xs:justify-end xs:w-1/2 dark:text-gray-400 dark:border-gray-800 dark:hover:text-primary-400 hover:text-primary-500"
-    >
-      <IconArrowLeft class="flex-shrink-0 order-first w-4 h-4 mr-2 xs:order-last xs:mr-0 xs:ml-2" />
-      <span class="truncate">{{ prev.title }}</span>
-    </NuxtLink>
-    <span v-else>&nbsp;</span>
+  <div v-if="prev || next" class="flex flex-col justify-between xs:space-x-2 py-4 font-medium leading-7 xs:flex-row">
+    <div class="w-full xs:w-1/2 flex justify-start">
+      <NuxtLink
+        v-if="prev"
+        :to="$contentLocalePath(prev.to)"
+        class="group inline-flex min-w-1/2 items-center justify-end w-full xs:w-auto px-4 py-4 truncate border border-gray-100 rounded xs:justify-end  dark:text-gray-400 dark:border-gray-800 dark:hover:text-primary-400 hover:text-primary-500 hover:border-primary-400 dark:hover:border-gray-700"
+      >
+        <span class="relative flex flex-col items-end truncate">
+          <span class="text-xs leading-tight  text-gray-400 group-hover:text-primary-400">{{ prev.category }}</span>
+          <span class="w-full leading-tight flex items-center"> 
+            <IconArrowLeft class="text-primary-500 flex-shrink-0 w-4 h-4 mr-2" />
+            <span class="truncate text-primary-500">{{ prev.title }}</span>
+          </span>
+        </span>
+      </NuxtLink>
+    </div>
 
-    <NuxtLink
-      v-if="next"
-      :to="$contentLocalePath(next.to)"
-      class="flex items-center justify-end w-full px-4 py-4 truncate border border-gray-100 rounded xs:justify-start xs:w-1/2 dark:text-gray-400 dark:border-gray-800 dark:hover:text-primary-400 hover:text-primary-500"
-    >
-      <IconArrowRight class="flex-shrink-0 order-last w-4 h-4 ml-2 xs:ml-0 xs:mr-2 xs:order-first" />
-      <span class="truncate">{{ next.title }}</span>
-    </NuxtLink>
-    <span v-else>&nbsp;</span>
+    <div class="order-first xs:order-last w-full xs:w-1/2 flex justify-end">
+      <NuxtLink
+        v-if="next"
+        :to="$contentLocalePath(next.to)"
+        class="group inline-flex mb-2 xs:mb-0 items-center justify-start w-full xs:w-auto min-w-1/2 px-4 py-4 truncate border border-gray-100 rounded xs:justify-start dark:text-gray-400 dark:border-gray-800 dark:hover:text-primary-400 hover:text-primary-500 hover:border-primary-400 dark:hover:border-gray-700"
+      >
+        <span class="relative flex flex-col items-start truncate">
+          <span class="text-xs leading-tight  text-gray-400 group-hover:text-primary-400">{{ next.category }}</span>
+          <span class="w-full leading-tight flex items-center">
+            <span class="truncate text-primary-500">{{ next.title }}</span>
+            <IconArrowRight class="text-primary-500 flex-shrink-0 w-4 h-4 ml-2" />
+          </span>
+        </span>
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
