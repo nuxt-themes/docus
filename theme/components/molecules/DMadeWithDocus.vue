d755002926cc9be311f112106c82dea7cfc22309
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2021-03-15 16:03:13 +0200

    feat: MadeWithDocus draft

---
+++ theme/components/molecules/DMadeWithDocus.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-gray-50 px-2 py-6 text-sm text-center">
+    Made with <a href="https://docus.dev" class="font-bold">DOCUS</a>
+  </div>
+</template>
--- theme/components/templates/AsideNavigation.vue
+++ theme/components/templates/AsideNavigation.vue
@@ -6,7 +6,9 @@
       class="w-auto h-full overflow-auto bg-white dark:bg-gray-900 lg:bg-transparent"
     >
       <!-- aside header -->
-      <div class="flex items-center w-full px-4 sm:px-6 lg:hidden h-18 bg-gray-50 dark:bg-gray-800">
+      <div
+        class="flex items-center w-full px-4 sm:px-6 lg:hidden h-18 bg-gray-50 dark:bg-gray-800"
+      >
         <button class="flex-1 focus:outline-none" @click.stop="menu = !menu">
           <IconArrowLeft class="w-5 h-5" />
         </button>
@@ -20,18 +22,22 @@
 
       <!-- aside navigation -->
       <nav
-        class="max-w-sm py-4 pr-24 pl-4 sm:pl-6 overflow-y-auto text-base font-medium h-(full-18) lg:pr-0 lg:text-sm lg:pt-10 lg:pb-16"
+        class="flex flex-col justify-between lg:justify-start max-w-sm overflow-y-auto text-base font-medium h-(full-18) lg:text-sm "
       >
-        <AsideTop />
-        <ul>
-          <DCategoryItem
-            v-for="(docs, category) in categories"
-            :key="category"
-            :category="category"
-            :docs="docs"
-          />
-        </ul>
-        <AsideBottom />
+        <div class="py-4 pr-24 pl-4 sm:pl-6 lg:pr-0 lg:pt-10 ">
+          <AsideTop />
+          <ul>
+            <DCategoryItem
+              v-for="(docs, category) in categories"
+              :key="category"
+              :category="category"
+              :docs="docs"
+            />
+          </ul>
+          <AsideBottom />
+        </div>
+  
+        <DMadeWithDocus v-if="$docus.settings.credits"/>
       </nav>
     </div>
   </div>
@@ -40,20 +46,20 @@
 <script>
 export default {
   computed: {
-    categories () {
-      return this.$docus.categories[this.$i18n.locale]
+    categories() {
+      return this.$docus.categories[this.$i18n.locale];
     },
-    lastRelease () {
-      return this.$docus.lastRelease
+    lastRelease() {
+      return this.$docus.lastRelease;
     },
     menu: {
-      get () {
-        return this.$menu.open
+      get() {
+        return this.$menu.open;
       },
-      set (val) {
-        this.$menu.open = val
-      }
-    }
-  }
-}
+      set(val) {
+        this.$menu.open = val;
+      },
+    },
+  },
+};
 </script>
