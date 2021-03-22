58ed7fe67358dd996cf723b810aaa5fef13ad410
Sébastien Chopin <seb@nuxtjs.com>
2021-03-22 18:44:44 +0100

    fix: ring not working

--- theme/components/dev/DocusUI.vue
+++ theme/components/dev/DocusUI.vue
@@ -6,20 +6,20 @@
     </h1>
     <ul class="p-2">
       <li class="flex items-center pb-1 font-medium">
-        <button type="button" class="relative inline-flex items-center justify-center flex-shrink-0 w-10 h-5 rounded-full cursor-pointer group focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" aria-pressed="false" @click="$docus.ui.draft = !$docus.ui.draft">
+        <button type="button" class="relative inline-flex items-center justify-center flex-shrink-0 w-10 h-5 rounded-full cursor-pointer group focus:outline-none" aria-pressed="false" @click="$docus.ui.draft = !$docus.ui.draft">
           <span class="sr-only">Show drafts</span>
           <span aria-hidden="true" class="absolute w-full h-full rounded-md pointer-events-none"></span>
           <span aria-hidden="true" :class="drafts ? 'bg-primary-500' : 'bg-gray-200'" class="absolute h-4 mx-auto transition-colors duration-200 ease-in-out rounded-full pointer-events-none w-9"></span>
-          <span aria-hidden="true" :class="drafts ? 'translate-x-5' : 'translate-x-0'" class="absolute left-0 inline-block w-5 h-5 transition-transform duration-200 ease-in-out transform bg-white border border-gray-200 rounded-full shadow pointer-events-none ring-0"></span>
+          <span aria-hidden="true" :class="drafts ? 'translate-x-5' : 'translate-x-0'" class="absolute left-0 inline-block w-5 h-5 transition-transform duration-200 ease-in-out transform bg-white border border-gray-200 rounded-full shadow pointer-events-none"></span>
         </button>
         <span class="ml-2 cursor-pointer" @click="$docus.ui.draft = !$docus.ui.draft">Drafts</span>
       </li>
       <li class="flex items-center font-medium">
-        <button type="button" class="relative inline-flex items-center justify-center flex-shrink-0 w-10 h-5 rounded-full cursor-pointer group focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" aria-pressed="false" @click="$docus.ui.slots = !$docus.ui.slots">
+        <button type="button" class="relative inline-flex items-center justify-center flex-shrink-0 w-10 h-5 rounded-full cursor-pointer group focus:outline-none" aria-pressed="false" @click="$docus.ui.slots = !$docus.ui.slots">
           <span class="sr-only">Show slots</span>
           <span aria-hidden="true" class="absolute w-full h-full rounded-md pointer-events-none"></span>
           <span aria-hidden="true" :class="slots ? 'bg-primary-500' : 'bg-gray-200'" class="absolute h-4 mx-auto transition-colors duration-200 ease-in-out rounded-full pointer-events-none w-9"></span>
-          <span aria-hidden="true" :class="slots ? 'translate-x-5' : 'translate-x-0'" class="absolute left-0 inline-block w-5 h-5 transition-transform duration-200 ease-in-out transform bg-white border border-gray-200 rounded-full shadow pointer-events-none ring-0"></span>
+          <span aria-hidden="true" :class="slots ? 'translate-x-5' : 'translate-x-0'" class="absolute left-0 inline-block w-5 h-5 transition-transform duration-200 ease-in-out transform bg-white border border-gray-200 rounded-full shadow pointer-events-none"></span>
         </button>
         <span class="ml-2 cursor-pointer" @click="$docus.ui.slots = !$docus.ui.slots">Slots</span>
       </li>
