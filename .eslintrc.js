144e26bb61b93498fe39a819a7668af552a1b1f3
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-06 15:47:09 +0200

    :wrench: (eslint) update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,30 @@ module.exports = {
     sourceType: 'module'
   },
   extends: [
-    '@nuxtjs'
+    '@nuxtjs',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
   ],
   rules: {
-    'vue/singleline-html-element-content-newline': 0,
-    'vue/multiline-html-element-content-newline': 0,
-    'vue/html-self-closing': 0,
-    'vue/no-v-html': 0
+    // Vue rules
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      {
+        registeredComponentsOnly: false
+      }
+    ],
+    'vue/singleline-html-element-content-newline': [0],
+    'vue/multiline-html-element-content-newline': [0],
+    'vue/html-self-closing': [0],
+    'vue/no-v-html': [0],
+    'vue/max-attributes-per-line': [0],
+    'vue/html-closing-bracket-newline': [0],
+    'vue/html-indent': [0],
+    // Prettier rules
+    'no-console': [1],
+    'space-before-function-paren': [0],
+    'arrow-parens': [0]
   }
 }
