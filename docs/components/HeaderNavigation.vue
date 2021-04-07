4942e49a4048280424b82396359e85c052d4e8bc
Sébastien Chopin <seb@nuxtjs.com>
2021-04-07 16:08:13 +0200

    chore: update header navigation padding

--- docs/components/HeaderNavigation.vue
+++ docs/components/HeaderNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full text-right">
+  <div class="w-full text-right pr-4">
     <NuxtLink v-show="$route.path === '/'" to="/get-started/installation" class="inline-flex items-center flex-none px-3 py-2 text-base font-semibold leading-4 text-white transition-colors duration-200 border border-transparent rounded bg-primary-500 hover:bg-primary-600">
       Get started
     </NuxtLink>
