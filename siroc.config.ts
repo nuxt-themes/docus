f73110684b72ce7f5dc4f6ec56ab30dd394289d5
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-14 01:36:55 +0200

    :wrench: (build) add types to dist via siroc hook

--- siroc.config.ts
+++ siroc.config.ts
@@ -1,13 +1,15 @@
 import fs from 'fs'
-
 import { glob as _glob } from 'glob'
 import pify from 'pify'
 import { defineSirocConfig } from 'siroc'
 import { minify, MinifyOptions } from 'terser'
+import { copyFileSync, mkdirSync, existsSync } from 'graceful-fs'
 
 const glob = pify(_glob)
 
 let messageSent = false
+
+let typesCopied = false
 
 export default defineSirocConfig({
   sortDependencies: true,
@@ -43,6 +45,26 @@ export default defineSirocConfig({
           await fs.promises.writeFile(file, code || contents)
         })
       )
+
+      // Copy types to /dist/types
+      if (!typesCopied) {
+        if (!existsSync('./dist/types')) mkdirSync('./dist/types')
+
+        const types = await glob('./src/types/*.ts')
+        const typesCount = types.length
+
+        await Promise.all(
+          types.map((file: string) => {
+            const fileName = file.split('/')[3]
+            return copyFileSync(file, `./dist/types/${fileName}`)
+          })
+        )
+
+        // eslint-disable-next-line no-console
+        console.log(`🏷Copying ${typesCount} type declarations`)
+
+        typesCopied = true
+      }
     }
   }
 })
