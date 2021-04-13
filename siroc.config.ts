086e0fb62cad79fef2f37aa023440d554a9ba00e
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-14 01:41:07 +0200

    :wrench: (build) export types via index.d.ts

--- siroc.config.ts
+++ siroc.config.ts
@@ -3,13 +3,10 @@ import { glob as _glob } from 'glob'
 import pify from 'pify'
 import { defineSirocConfig } from 'siroc'
 import { minify, MinifyOptions } from 'terser'
-import { copyFileSync, mkdirSync, existsSync } from 'graceful-fs'
 
 const glob = pify(_glob)
 
 let messageSent = false
-
-let typesCopied = false
 
 export default defineSirocConfig({
   sortDependencies: true,
@@ -41,30 +38,11 @@ export default defineSirocConfig({
       await Promise.all(
         jsFiles.map(async (file: string) => {
           const contents = await fs.promises.readFile(file, 'utf-8')
+          // @ts-ignore
           const { code } = minify(contents, options)
           await fs.promises.writeFile(file, code || contents)
         })
       )
-
-      // Copy types to /dist/types
-      if (!typesCopied) {
-        if (!existsSync('./dist/types')) mkdirSync('./dist/types')
-
-        const types = await glob('./src/types/*.ts')
-        const typesCount = types.length
-
-        await Promise.all(
-          types.map((file: string) => {
-            const fileName = file.split('/')[3]
-            return copyFileSync(file, `./dist/types/${fileName}`)
-          })
-        )
-
-        // eslint-disable-next-line no-console
-        console.log(`🏷Copying ${typesCount} type declarations`)
-
-        typesCopied = true
-      }
     }
   }
 })
