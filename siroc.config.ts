018b4b6875faf2972683aa053592d16c9da930bd
Daniel Roe <daniel@roe.dev>
2021-04-13 23:43:06 +0100

    build: (temporarily) minify dist files (#249)
    * build: (temporarily) minify dist files
    
    * :wrench: (siroc) update tersing to only JS and output files number
    
    Co-authored-by: Yaël GUILLOUX <yael.guilloux@gmail.com>

--- siroc.config.ts
+++ siroc.config.ts
@@ -1,8 +1,48 @@
+import fs from 'fs'
+
+import { glob as _glob } from 'glob'
+import pify from 'pify'
 import { defineSirocConfig } from 'siroc'
+import { minify, MinifyOptions } from 'terser'
+
+const glob = pify(_glob)
+
+let messageSent = false
 
 export default defineSirocConfig({
   sortDependencies: true,
   rollup: {
     externals: ['./src/app/nuxt.config.js']
+  },
+  hooks: {
+    // Minify build
+    async 'build:done'() {
+      // Wait for mkdist to transpile everything
+      await new Promise(resolve => setTimeout(resolve, 2000))
+
+      const jsFiles = await glob('./dist/**/*.js')
+      const nameCache = {}
+      const options: MinifyOptions = {
+        nameCache,
+        ecma: 2020
+      }
+
+      if (!messageSent) {
+        const filesCount = jsFiles.length
+
+        // eslint-disable-next-line no-console
+        console.log(`🔐Minifying ${filesCount} built files`)
+
+        messageSent = true
+      }
+
+      await Promise.all(
+        jsFiles.map(async (file: string) => {
+          const contents = await fs.promises.readFile(file, 'utf-8')
+          const { code } = minify(contents, options)
+          await fs.promises.writeFile(file, code || contents)
+        })
+      )
+    }
   }
 })
