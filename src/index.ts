5b7752b47c27f9b0d24acee4e11edb291aa75852
Daniel Roe <daniel@roe.dev>
2021-04-13 11:12:17 +0100

    fix: use jiti to require nuxt config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import type { NuxtConfig } from '@nuxt/types'
 import defu from 'defu'
 
+import jiti from 'jiti'
+const _require = jiti(__filename, { interopDefault: true })
+
 export function withDocus(userConfig: NuxtConfig): NuxtConfig {
-  const nuxtConfig = require('./app/nuxt.config')
+  const nuxtConfig = _require('./app/nuxt.config')
 
   const config = defu.arrayFn(userConfig, nuxtConfig)
 
