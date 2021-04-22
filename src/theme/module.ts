0d1666334e3b2deac27117bd62c55de4a625855c
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-22 21:26:03 +0200

    :fire: (theme) remove theme module, unnecessary

--- src/theme/module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nuxt/types'
-
-export default <Module>function docusThemeModule() {
-  const { nuxt, requireModule } = this
-  const { options } = nuxt
-
-  if (options.theme && options.theme.path) requireModule(options.theme.path)
-  else requireModule('../defaultTheme')
-}
