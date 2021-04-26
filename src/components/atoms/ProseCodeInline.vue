213ea2553f918b4dea148939f993ad6179ed3aa6
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2021-04-26 18:44:05 +0300

    feat: ProseCodeInline component

--- src/components/atoms/ProseCodeInline.vue
+++ src/components/atoms/ProseCodeInline.vue
@@ -4,4 +4,20 @@
   </code>
 </template>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+code {
+  color: inherit;
+  font-weight: 400;
+  font-size: 0.875em;
+  padding: 0.25rem;
+  background-color: theme('colors.gray.100');
+  border-radius: 0.375rem;
+}
+
+.dark code {
+  font-weight: 400;
+  color: inherit;
+  background-color: theme('colors.gray.800');
+  border-radius: 0.375rem;
+}
+</style>
