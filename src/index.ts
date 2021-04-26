e2931add51cf93640d2448ac3aa107e0b7a4be66
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-26 10:23:46 +0200

    :sparkles: (withDocus) improve function

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,8 @@ const _require = jiti(__filename)
 
 export function withDocus(userConfig: NuxtConfig): NuxtConfig {
   const _config = _require('./app/nuxt.config')
-  const _nuxtConfig = _config.default || _config
 
-  const config = nuxtConfig(defu.arrayFn(userConfig, _nuxtConfig))
+  const _nuxtConfig = (_config.default || _config) as NuxtConfig
 
-  return config as NuxtConfig
+  return nuxtConfig(defu.arrayFn(userConfig, _nuxtConfig))
 }
