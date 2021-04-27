5767ca77dc646ec0a683e240c486f7ded3e0c5ed
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-27 02:32:22 +0200

    :sparkles: (settings) generate docus-settings file after parsing settings (wip)

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,22 +1,41 @@
-import { resolve } from 'path'
+import { join, resolve } from 'path'
+import { mkdirp, remove, existsSync, writeFileSync } from 'fs-extra'
 import { Module } from '@nuxt/types'
-import { useDefaults } from './settings'
+import { useDefaults, useDefaultsTheme } from './settings'
 
-export default <Module>function settingsModule() {
+export default <Module>async function settingsModule() {
   const { nuxt } = this
   const { options } = nuxt
 
+  const cacheDir = join(options.rootDir, 'node_modules/.cache/docus')
+
+  // Inject content dir in private runtime config
+  const contentDir = options?.content?.dir || 'content'
+
+  options.publicRuntimeConfig.contentDir = contentDir
+
   // Read Docus settings
-  const settingsPath = resolve(options.srcDir, 'settings.json')
+  let settingsPath = resolve(options.srcDir, 'docus.config')
+
+  if (existsSync(settingsPath + '.js')) settingsPath += '.js'
+  if (existsSync(settingsPath + '.ts')) settingsPath += '.ts'
 
   try {
-    const userSettings = require(settingsPath)
+    const { theme, ...userSettings } = require(settingsPath)
     const settings = useDefaults(userSettings)
+    settings.theme = useDefaultsTheme(theme)
 
-    // default title and description for pages
+    // Default title and description for pages
     options.meta.name = settings.title
     options.meta.description = settings.description
+
+    // Replace the directory
+    if (existsSync(cacheDir)) await remove(cacheDir)
+    await mkdirp(cacheDir)
+    // Write settings
+    writeFileSync(join(cacheDir, 'docus-settings.json'), JSON.stringify(settings))
   } catch (err) {
-    /* settings not found */
+    console.log(err)
+    console.log('Settings not found!')
   }
 }
