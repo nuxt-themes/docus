c336ae95ec47d1bc9fd1dfdbc301191b3bf38df9
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-27 14:57:19 +0200

    :sparkles: (docus) resolve theme from withDocus helper
    we cannot rewrite extending at build time, so we have to resolve theme from here until it is possible

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { nuxtConfig } from 'nuxt-extend'
 import type { NuxtConfig } from '@nuxt/types'
 import defu from 'defu'
@@ -5,10 +6,24 @@ import jiti from 'jiti'
 
 const _require = jiti(__filename)
 
-export function withDocus(userConfig: NuxtConfig): NuxtConfig {
+export function withDocus(userConfig: NuxtConfig, theme = undefined): NuxtConfig {
   const _config = _require('./app/nuxt.config')
 
   const _nuxtConfig = (_config.default || _config) as NuxtConfig
+
+  // Resolve theme path
+  if (!_nuxtConfig.extends && !theme) {
+    // No theme specified, load default one
+    _nuxtConfig.extends = resolve(__dirname, './defaultTheme')
+  } else {
+    // Theme specified, try to extend from its index
+    _nuxtConfig.extends = theme
+  }
+
+  // Broadcast the currently used theme
+  const pathSplit = _nuxtConfig.extends.split('/')
+  // eslint-disable-next-line no-console
+  console.log(`💄 Using Docus theme: ${pathSplit[pathSplit.length - 1]}`)
 
   return nuxtConfig(defu.arrayFn(userConfig, _nuxtConfig))
 }
