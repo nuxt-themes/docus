2e8540f856a93dfcb6051bbfafc272eac728fe09
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-27 02:30:38 +0200

    :sparkles: (docus) add docus.config.ts

---
+++ docs/docus.config.ts
@@ -0,0 +1,19 @@
+export default {
+  title: 'Docus',
+  description: 'Write in markdown, use Vue components, add style with Windi CSS and enjoy the power of Nuxt.',
+  url: 'https://docus.dev',
+  twitter: 'nuxtlabs',
+  template: 'docs',
+  credits: true,
+  github: {
+    repo: 'nuxtlabs/docus',
+    branch: 'main',
+    dir: 'docs',
+    releases: true
+  },
+  _algolia: {
+    apiKey: 'd8bb34f345ca54362176cf78fcf4ed9d',
+    indexName: 'docus'
+  },
+  theme: {}
+}
