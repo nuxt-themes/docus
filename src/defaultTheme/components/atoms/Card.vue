98d74276df4435ed965e4bd2570d616a20ffb300
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-28 22:42:59 +0200

    :twisted_rightwards_arrows: (theme) update components after merge

--- src/defaultTheme/components/atoms/Card.vue
+++ src/defaultTheme/components/atoms/Card.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex">
     <div
-      class="relative w-full px-6 py-8 bg-white rounded border border-gray-200 dark:border-gray-700 dark:bg-gray-900"
+      class="relative w-full px-6 py-8 bg-white border border-gray-200 rounded dark:border-gray-700 dark:bg-gray-900"
     >
       <InjectComponent :component="icon" class="w-16 h-16 mb-3" :class="iconClass || ''">
-        <img v-if="isImage(icon)" :src="icon" class="inline-block w-16 h-16 mb-3" />
+        <NuxtImg v-if="isImage(icon)" :src="icon" class="inline-block w-16 h-16 mb-3" />
         <span v-else class="inline-block w-16 h-16 mb-3 text-6xl">{{ icon }}</span>
       </InjectComponent>
       <h3 class="mb-2 text-xl">{{ title }}</h3>
