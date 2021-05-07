871f54d9fdb0e1e1636bab2b9248c8a0406564b8
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-07 15:35:04 +0200

    :sparkles: (admin) update utils

--- src/admin/app/utils.ts
+++ src/admin/app/utils.ts
@@ -1,3 +1,3 @@
 const IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif', '.svg']
 
-export const isImage = file => file && IMG_EXTENSIONS.includes(file.extension)
+export const isImage = (file: any) => file && IMG_EXTENSIONS.includes(file.extension)
