34a24c98f384a6bd5c1e5c0b69f706531f7c629b
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-07 21:21:48 +0200

    :sparkles: (admin) cleanup editor

--- src/admin/app/components/Editor.vue
+++ src/admin/app/components/Editor.vue
@@ -1,39 +1,65 @@
 <template>
-  <textarea v-model="value" class="w-full h-full" />
+  <textarea v-model="frontmatter" class="h-24 w-full" />
+  <textarea v-model="content" class="w-full h-full" />
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue3'
+import { GrayMatterFile } from 'gray-matter'
+import { defineComponent, computed, ref, watch, PropType } from 'vue3'
 import { useApi } from '../plugins/api'
+
+type PermissiveGrayMatterFile = GrayMatterFile<any> & { file: any; path: any }
 
 export default defineComponent({
   props: {
     file: {
-      type: Object,
-      required: true
-    },
-    modelValue: {
-      type: String,
+      type: Object as PropType<PermissiveGrayMatterFile>,
       required: true
     }
   },
-  setup(props, { emit }) {
+  setup(props) {
     const api = useApi()
 
-    const value = computed({
-      get() {
-        return props.modelValue
-      },
-      set(value) {
-        emit('update:modelValue', value)
+    // Sync local data when file changes
+    watch(
+      () => props.file,
+      newVal => {
+        data.value = newVal.data
+        content.value = newVal.content
+      }
+    )
 
-        api.put(`/pages${props.file.path}`, {
-          data: value
-        })
+    // Local data
+    const data = ref(props.file.data)
+    const content = ref(props.file.content)
+
+    // Stringified reference for frontmatter text-area
+    const frontmatter = computed({
+      get() {
+        return JSON.stringify(data.value, null, 2)
+      },
+      set(value: string) {
+        try {
+          data.value = JSON.parse(value)
+        } catch (e) {
+          // New value is not a valid JSON string.
+          // Do nothing and wait for the next valid JSON input.
+        }
       }
     })
 
-    return { value }
+    // API update on change on data or content
+    watch([data, content], () => {
+      api.put(`/pages${props.file.path}`, {
+        data: data.value,
+        content: content.value
+      })
+    })
+
+    return {
+      frontmatter,
+      content
+    }
   }
 })
 </script>
