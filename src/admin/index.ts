e8cd16edd01a3adbcdf498f5220f7d2a2739d800
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-07 21:22:45 +0200

    :sparkles: (admin) try to implement admin via proxy

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -1,18 +1,18 @@
-import { join } from 'path'
 import { execSync } from 'child_process'
+import { join } from 'upath'
 import chalk from 'chalk'
-import serveStatic from 'serve-static'
 import { NuxtOptionsServer } from '@nuxt/types/config/server'
+import serveStatic from 'serve-static'
 import { Module } from '@nuxt/types'
 import api from './api'
 
 export default <Module>function () {
-  const { nuxt } = this
+  const { nuxt, addServerMiddleware } = this
 
   process.options = nuxt.options
-  process.previewUrl = 'http://localhost:3000'
+  process.previewUrl = 'http://localhost:4000'
 
-  this.addServerMiddleware({
+  addServerMiddleware({
     path: '/admin/api',
     handler: api
   })
@@ -24,6 +24,29 @@ export default <Module>function () {
     path: '/admin/',
     handler: serveStatic(join(__dirname, 'app/dist'))
   })
+
+  // TODO: Implement Vite as server middleware/proxy
+  //       instead of using the built version.
+  /* if (process.env.ADMIN_DEV && false) {
+    // Start the admin in development
+    const viteServer = await createServer({
+      configFile: false,
+      ...viteConfig
+    })
+    await viteServer.listen()
+    const viteUrl = `http://localhost:${viteServer.config.server.port}`
+
+    // TODO: handle when Array syntax
+    const proxy = (this.options.proxy = this.options.proxy || {})
+    proxy['/admin/'] = {
+      target: viteUrl,
+      pathRewrite: {
+        '^/admin/': '/'
+      }
+    }
+
+    this.requireModule('@nuxtjs/proxy')
+  } */
 
   nuxt.hook('listen', (_: any, { host, port }: NuxtOptionsServer) => {
     process.previewUrl = `http://${host}:${port}`
