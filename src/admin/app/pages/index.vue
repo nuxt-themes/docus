fd2a221c1e81641720c47927a7d641a1518d6409
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-07 14:54:36 +0200

    :sparkles: (admin) cleanup index

--- src/admin/app/pages/index.vue
+++ src/admin/app/pages/index.vue
@@ -1,20 +1,12 @@
 <template>
-  <div class="flex flex-shrink-0">
-    <div class="flex flex-col w-64">
-      <!-- Sidebar component, swap this element with another sidebar if you like -->
-      <div class="flex flex-col flex-grow overflow-y-auto bg-white border-r border-gray-r-200">
-        <div class="flex flex-col flex-grow mt-2">
-          <nav class="flex-1 px-2 space-y-1 bg-white">
-            <FilesTree :files="files" :current-file="currentFile" @open="openFile" />
-          </nav>
-        </div>
-      </div>
-    </div>
-  </div>
+  <aside class="w-64 p-2 relative border-r">
+    <FilesTree :files="files" :current-file="currentFile" @open="openFile" />
+  </aside>
 
   <!-- eslint-disable-next-line vue/no-multiple-template-root -->
-  <div class="flex flex-col flex-1 overflow-hidden border-r">
+  <div class="flex-1 overflow-y-scroll border-r">
     <Editor v-if="currentFile" v-model="currentFile.data" :file="currentFile" />
+
     <p v-else class="p-4 text-gray-700">👈 &nbsp;Select a file to edit.</p>
   </div>
 </template>
