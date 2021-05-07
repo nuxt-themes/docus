d2f0db8be3838a59524577c49e0682532ab1bb1b
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-07 14:53:13 +0200

    :sparkles: (admin) refact header

--- src/admin/app/components/AppHeader.vue
+++ src/admin/app/components/AppHeader.vue
@@ -1,28 +1,12 @@
 <template>
-  <header class="relative z-10 flex items-center flex-none border-b">
-    <div class="flex items-center h-12 px-4 border-r">
-      <h1>
-        Docus
-        <span class="px-2 py-1 ml-1 text-xs font-medium bg-gray-200 rounded">ADMIN</span>
-      </h1>
-    </div>
+  <header class="relative z-10 flex items-center border-b">
+    <AppHeaderLogo />
 
-    <div class="flex items-center flex-auto">
-      <div class="flex items-center ml-4 space-x-2">
-        <RouterLink to="/" class="relative flex-none px-4 py-1 text-sm font-medium leading-5 border rounded-md">
-          Pages
-        </RouterLink>
-
-        <RouterLink to="/static" class="relative flex-none px-4 py-1 text-sm font-medium leading-5 border rounded-md">
-          Static
-        </RouterLink>
-      </div>
-    </div>
+    <AppHeaderNav />
   </header>
 </template>
 
-<style lang="postcss" scoped>
-.router-link-active {
-  @apply border-blue-500 bg-blue-100;
-}
-</style>
+<script setup>
+import AppHeaderLogo from './AppHeaderLogo.vue'
+import AppHeaderNav from './AppHeaderNav.vue'
+</script>
---
+++ src/admin/app/components/AppHeaderNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center ml-4 space-x-2 flex-auto">
+    <RouterLink to="/" class="relative flex-none px-4 py-1 text-sm font-medium leading-5 border rounded-md">
+      Pages
+    </RouterLink>
+
+    <RouterLink
+      to="/static"
+      class="relative flex-none px-4 py-1 text-sm font-medium leading-5 border rounded-md"
+      :class="[$route.path.includes('static') ? 'router-link-active' : '']"
+    >
+      Static
+    </RouterLink>
+  </div>
+</template>
+
+<style lang="postcss">
+.router-link-active {
+  @apply border-blue-500 bg-blue-100;
+}
+</style>
