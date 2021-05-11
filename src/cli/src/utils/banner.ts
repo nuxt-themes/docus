5bb24535aabe8baaa2e7ae1c21172b8d0877d89e
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-12 01:31:10 +0200

    :sparkles: (cli) import utils from @nuxt/cli

---
+++ src/cli/src/utils/banner.ts
@@ -0,0 +1,11 @@
+import clear from 'clear'
+import { green } from 'colorette'
+import { version } from '../../package.json'
+
+export function showBanner(_clear: boolean) {
+  if (_clear) {
+    clear()
+  }
+
+  console.log(green(`Docus CLI v${version}`))
+}
