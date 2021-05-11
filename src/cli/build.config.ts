0ad03b6cc3db3338b3708df1754db658865d31bf
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-12 01:29:13 +0200

    :sparkles: (cli) create docus build config

---
+++ src/cli/build.config.ts
@@ -0,0 +1,8 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  declaration: false,
+  inlineDependencies: true,
+  entries: ['src/index'],
+  externals: ['@nuxt/kit', 'fsevents']
+})
