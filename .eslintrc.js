2fba70b9b614643db9e17282f21c60acccc2bdd3
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-12 17:57:52 +0200

    :wrench: (eslint) update eslint config (w/ comments)

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,22 @@ module.exports = {
     'space-before-function-paren': [0],
     'arrow-parens': [0],
     curly: [0],
-    'keyword-spacing': [0]
+    'keyword-spacing': [0],
+    // TODO: Remove all configs below
+    //
+    // This is done in order to avoid src/admin/app
+    // Errors on multiple roots, as this is a Vue 3 project
+    // And Vue 3 allows multiple roots.
+    //
+    // Note:
+    // If you still have the error displayed, it means Vetur is trying
+    // To lint your file with Vue 2 configuration, to avoid that
+    // Add:
+    // 'vetur.validation.template': false,
+    // 'vetur.validation.script': false,
+    // 'vetur.validation.style': false,
+    // To your `settings.json`, from VSCode.
+    //
+    'vue/no-multiple-template-root': 0
   }
 }
