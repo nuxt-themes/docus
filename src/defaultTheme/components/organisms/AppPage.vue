acc69ec01cb0c50def7fdec1d2bafa2d70524c50
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-26 19:49:09 +0200

    :fire: (theme) update AppPage

--- src/defaultTheme/components/organisms/AppPage.vue
+++ src/defaultTheme/components/organisms/AppPage.vue
@@ -1,24 +1,9 @@
 <template>
   <div class="flex flex-col w-full pt-0 xl:flex-row xl:pt-10">
-    <article
-      class="flex-auto order-last min-w-0 px-4 sm:px-6 mt-4 xl:order-first sm:px-6 xl:mt-0"
-      :class="prose ? 'prose dark:prose-dark' : ''"
-    >
+    <article class="flex-auto order-last min-w-0 px-4 sm:px-6 mt-4 xl:order-first sm:px-6 xl:mt-0">
       <slot />
     </article>
+
     <slot name="toc" />
   </div>
 </template>
-
-<script>
-import { defineComponent } from '@nuxtjs/composition-api'
-
-export default defineComponent({
-  props: {
-    prose: {
-      type: Boolean,
-      default: false
-    }
-  }
-})
-</script>
