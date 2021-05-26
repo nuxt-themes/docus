0018bf7a3520dfd885004b2b0aa2eeaaf5ee2cbb
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-26 19:49:26 +0200

    :sparkles: (docus) create AppTemplate

---
+++ src/defaultTheme/components/organisms/AppTemplate.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="lg:flex" :class="fluid ? '' : 'd-container'">
+    <slot v-if="true" name="aside"><AppAside /></slot>
+
+    <div class="flex-auto w-full min-w-0 lg:static lg:max-h-full lg:overflow-visible">
+      <slot />
+    </div>
+  </div>
+</template>
