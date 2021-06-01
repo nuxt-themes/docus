c566681fb4a008f05d286847ac065609a3d4cc51
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2021-06-02 02:24:49 +0300

    feat: marketing template (#317)
    * feat: init Pricing template
    
    * feat: Pricing template
    
    * fix: improvements
    
    * fix: update pricing
    
    * feat: InfoLabel component

--- src/defaultTheme/components/atoms/icons/IconCheck.vue
+++ src/defaultTheme/components/atoms/icons/IconCheck.vue
@@ -1,11 +1,5 @@
 <template>
-  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M19.5 6L9 18L4.5 13.5"
-      stroke="currentColor"
-      stroke-width="1.55625"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
   </svg>
 </template>
---
+++ src/defaultTheme/components/templates/Marketing.vue
@@ -0,0 +1,18 @@
+<template>
+  <AppContainer fluid :footer="page.footer">
+    <NuxtContent :document="page" />
+  </AppContainer>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    page: {
+      type: Object,
+      required: true
+    }
+  }
+})
+</script>
