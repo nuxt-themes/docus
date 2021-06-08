1a6f4bf38e57d636ac61cecf9a9439fe2ed7ecf1
Ahad Birang <farnabaz@gmail.com>
2021-06-08 18:25:12 +0430

    fix(core): do not wrap blocks inside paragraph (#390)
    * fix(core): do not wrapp blocks inside paragraph
    
    * chore: rename inline directive

--- src/core/parser/markdown/directive/micromark-directive/index.ts
+++ src/core/parser/markdown/directive/micromark-directive/index.ts
@@ -1,13 +1,13 @@
 // https://github.com/micromark/micromark-extension-directive/blob/main/lib/syntax.js
 
-import directiveText from './tokenize-directive-text'
+import directiveInline from './tokenize-directive-inline'
 import directiveContainer from './tokenize-directive-container'
 import directiveContainerIndented from './tokenize-directive-container-indented'
 
 export default function directive() {
   return {
-    text: { 58: directiveText },
-    flow: { 58: [directiveContainer] },
+    text: { 58: directiveInline },
+    flow: { 58: [directiveContainer, directiveInline] },
     flowInitial: {
       '-2': directiveContainerIndented,
       '-1': directiveContainerIndented,
