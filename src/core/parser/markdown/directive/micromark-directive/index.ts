4dbdc05cc3be7ad38cbdfd5ffda02e60cbe037b7
Farnabaz <farnabaz@gmail.com>
2021-06-08 18:16:02 +0430

    fix(core): do not wrapp blocks inside paragraph

--- src/core/parser/markdown/directive/micromark-directive/index.ts
+++ src/core/parser/markdown/directive/micromark-directive/index.ts
@@ -7,7 +7,7 @@ import directiveContainerIndented from './tokenize-directive-container-indented'
 export default function directive() {
   return {
     text: { 58: directiveText },
-    flow: { 58: [directiveContainer] },
+    flow: { 58: [directiveContainer, directiveText] },
     flowInitial: {
       '-2': directiveContainerIndented,
       '-1': directiveContainerIndented,
