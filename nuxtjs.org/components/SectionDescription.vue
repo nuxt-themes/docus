e181fc51bdc00ec9b81171ccf96b8361b7c48b9a
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2021-06-08 20:33:18 +0300

    move hero parallax

--- nuxtjs.org/components/SectionDescription.vue
+++ nuxtjs.org/components/SectionDescription.vue
@@ -1,32 +1,5 @@
 <template>
-  <ALabel
-    tag="p"
-    class="font-normal leading-relaxed"
-    :class="{
-      'text-sm 2xl:text-md': size === 'sm',
-      'text-md lg:text-lg 2xl:text-lg': size === 'md',
-      'text-lg 2xl:text-xl': size === 'lg'
-    }"
-  >
+  <p class="font-normal xl:text-body-xl 2xl:text-body-2xl">
     <slot />
-  </ALabel>
+  </p>
 </template>
-
-<script>
-import { defineComponent } from '@nuxtjs/composition-api'
-export default defineComponent({
-  props: {
-    tag: {
-      type: String,
-      default: 'p'
-    },
-    size: {
-      type: String,
-      default: 'lg',
-      validator(value) {
-        return ['sm', 'md', 'lg'].includes(value)
-      }
-    }
-  }
-})
-</script>
