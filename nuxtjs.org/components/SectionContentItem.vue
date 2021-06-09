1b3d096c4642a8aaafbec6e7a36f7832bb8d60b7
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2021-06-09 19:04:20 +0300

    section content item

---
+++ nuxtjs.org/components/SectionContentItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <div class="flex flex-col items-center">
+      <slot name="image">
+        <img class="mb-4" :src="image" :alt="`A ${title} image`" />
+      </slot>
+      <h3 class="mb-1 text-center text-body-lg lg:text-body-xl font-bold">{{ title }}</h3>
+      <p class="text-center text-sm lg:text-base">{{ description }}</p>
+    </div>
+  </li>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    image: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
