f81f1291311751903b5d20f617441de33053eff9
Sébastien Chopin <seb@nuxtjs.com>
2021-06-10 16:29:27 +0200

    docs: NuxtJS -> Nuxt

--- nuxtjs.org/components/HomeCommunity.vue
+++ nuxtjs.org/components/HomeCommunity.vue
@@ -100,7 +100,7 @@ export default defineComponent({
       },
       {
         testimonial:
-          'NuxtJS has outstanding developer productivity, experience, and performance right out of the gate!<br/>There’s so much attention to detail, ensuring teams have everything at their fingertips to productively build all manners of applications.',
+          'Nuxt has outstanding developer productivity, experience, and performance right out of the gate!<br/>There’s so much attention to detail, ensuring teams have everything at their fingertips to productively build all manners of applications.',
         author: 'Sarah Drasner',
         job: 'Core Team of Vue.js',
         icon_author: 'sarah',
@@ -108,7 +108,7 @@ export default defineComponent({
       },
       {
         testimonial:
-          'NuxtJS is a fantastic choice for teams building a production-grade product on the web. It aims to bake in performance best-practices while maintaining excellent Vue.js DX.',
+          'Nuxt is a fantastic choice for teams building a production-grade product on the web. It aims to bake in performance best-practices while maintaining excellent Vue.js DX.',
         author: 'Addy Osmani',
         job: 'Chief Engineer of Chrome',
         icon_author: 'addy',
