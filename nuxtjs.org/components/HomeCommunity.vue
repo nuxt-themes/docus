e8ba3c5c92c4b7ffd92fea6d481a3c2e56f8356e
Sébastien Chopin <seb@nuxtjs.com>
2021-06-10 14:57:04 +0200

    chore: update testimnonials

--- nuxtjs.org/components/HomeCommunity.vue
+++ nuxtjs.org/components/HomeCommunity.vue
@@ -18,47 +18,51 @@
     <template #section-content>
       <SectionContent class="col-span-12 items-center pt-20 pb-60">
         <template #category>
-          <ALabel tag="span" class="text-primary-green font-bold text-lg"
-            >Community</ALabel
-          >
+          <ALabel tag="span" class="text-primary-green font-bold text-lg">Community</ALabel>
         </template>
         <template #title>
-          <SectionTitle class="text-center font-serif" size="sm"
-            >Developer love it</SectionTitle
-          >
+          <SectionTitle class="text-center font-serif" size="sm">Developer love it</SectionTitle>
         </template>
         <template #paragraph>
           <SectionDescription class="pb-16 text-center" size="md"
-            >Be part of the Nuxt.js community and help us Nuxtify the
-            world.</SectionDescription
+            >Be part of the Nuxt.js community and help us Nuxtify the world.</SectionDescription
           >
         </template>
         <template #content>
-          <ul
-            class="grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-8 lg:gap-20 pb-8 sm:mx-20 md:mx-0"
-          >
+          <ul class="grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-8 lg:gap-20 pb-8 sm:mx-20 md:mx-0">
             <li
               v-for="testimonial in testimonials"
               :key="testimonial.author"
-              class="relative flex flex-col items-center space-y-3 border-3 border-gray-200 rounded-lg px-4 pb-6"
+              class="
+                relative
+                flex flex-col
+                items-center
+                justify-between
+                space-y-4
+                border-3 border-gray-200
+                rounded-lg
+                p-4
+              "
             >
-              <img
-                :src="`/img/home/community/${testimonial.icon_author}.png`"
-                class="absolute h-12 bottom-0 -mb-9"
-              />
-              <img
-                :src="`/img/home/community/${testimonial.icon_job}.svg`"
-                class="md:hidden lg:block absolute h-7 bottom-5 right-0 mr-4 mb-4"
-              />
-              <ALabel tag="p" class="text-center px-4 xl:px-16 2xl:px-12">{{
-                testimonial.testimonial
-              }}</ALabel>
-              <ALabel tag="span" class="font-bold text-center text-base">{{
-                testimonial.author
-              }}</ALabel>
-              <ALabel tag="span" class="text-center text-sm">{{
-                testimonial.job
-              }}</ALabel>
+              <ALabel tag="p" class="text-left" v-html="testimonial.testimonial"></ALabel>
+              <div class="flex w-full justify-between align-middle">
+                <img
+                  :src="`/img/home/community/${testimonial.icon_author}.png`"
+                  width="48"
+                  height="48"
+                  class="h-12 w-12"
+                />
+                <div class="flex flex-1 pl-4 text-left flex-col">
+                  <ALabel tag="span" class="font-bold text-base">{{ testimonial.author }}</ALabel>
+                  <ALabel tag="span" class="text-sm">{{ testimonial.job }}</ALabel>
+                </div>
+                <img
+                  :src="`/img/home/community/${testimonial.icon_job}.svg`"
+                  width="28"
+                  height="28"
+                  class="hidden xl:block"
+                />
+              </div>
             </li>
           </ul>
         </template>
@@ -87,8 +91,7 @@ export default defineComponent({
   setup() {
     const testimonials = [
       {
-        testimonial:
-          'Just migrated a few SPAs to @NuxtJS. Literally impossible to make the process any easier. Super impressive.',
+        testimonial: 'Not bad :)',
         author: 'Evan You',
         icon_author: 'evan',
         job: 'Creator of VueJS',
@@ -96,18 +99,18 @@ export default defineComponent({
       },
       {
         testimonial:
-          'Just migrated a few SPAs to @NuxtJS. Literally impossible to make the process any easier. Super impressive.',
+          'NuxtJS has outstanding developer productivity, experience, and performance right out of the gate!<br/>There’s so much attention to detail, ensuring teams have everything at their fingertips to productively build all manners of applications.',
         author: 'Sarah Drasner',
-        job: 'Core Team of VueJS',
+        job: 'Core Team of Vue.js',
         icon_author: 'sarah',
         icon_job: 'vue'
       },
       {
         testimonial:
-          'Just migrated a few SPAs to @NuxtJS. Literally impossible to make the process any easier. Super impressive.',
+          'NuxtJS is a fantastic choice for teams building a production-grade product on the web. It aims to bake in performance best-practices while maintaining excellent Vue.js DX.',
         author: 'Addy Osmani',
         job: 'Chief Engineer of Chrome',
-        icon_author: 'osman',
+        icon_author: 'addy',
         icon_job: 'google'
       }
     ]
