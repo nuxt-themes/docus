31fbcd00f5e13486d20d88eca32700913760600c
Florent <flo@nuxtjs.com>
2021-06-11 11:59:18 +0200

    :sparkles: (iconAlert) update svg

--- nuxtjs.org/components/IconAlertsuccess.vue
+++ nuxtjs.org/components/IconAlertsuccess.vue
@@ -7,6 +7,6 @@
     viewBox="0 0 24 24"
     stroke="currentColor"
   >
-    <path d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+    <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
   </svg>
 </template>
--- nuxtjs.org/components/IconAlertwarning.vue
+++ nuxtjs.org/components/IconAlertwarning.vue
@@ -1,9 +1,12 @@
 <template>
-  <svg fill="currentColor" viewBox="0 0 20 20" class="h-5 w-5 alert-icon">
-    <path
-      fill-rule="evenodd"
-      d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-      clip-rule="evenodd"
-    ></path>
+  <svg
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
   </svg>
 </template>
