b9243a1faf523f1597e2fb206cbacb4381aaf2e4
Florent Delerue <flo@nuxtjs.com>
2021-06-16 17:08:31 +0200

    feat: team page (#442)
    * :ok_hand: (Alert) CapitalCamelCase on IconAlert
    
    * :ok_hand: (docs) use docus syntax for nav title
    
    * :ok_hand: (docs) use docus syntax for nav title
    
    * :ok_hand: (examples) csb_link moved to ::code-sandbox component
    
    * :construction: (team page)
    
    * :sparkles: (teams) add core and community md files
    
    * :wrench: (windi.config) add sky color
    
    * :sparkles: (teams) teams page
    
    * :bug: (TeamMember) fix invalid key on v-for
    
    * :bug: (teams) change title to name

---
+++ nuxtjs.org/components/TeamSection.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="py-16 d-container-content">
+    <div v-for="team in teams" :key="team.slug">
+      <TeamTitle :slug="team.slug" :name="team.name" class="text-center pb-16" />
+      <div class="flex justify-center">
+        <div class="flex flex-wrap justify-center pb-16">
+          <div v-for="member in team.members" :key="member.name">
+            <TeamMember :member="member" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, useContext, useFetch } from '@nuxtjs/composition-api'
+export default defineComponent({
+  setup() {
+    const { $docus } = useContext()
+    const teams = ref()
+    useFetch(async () => {
+      teams.value = await $docus
+        .search('/teams', { deep: true })
+        .where({ slug: { $in: ['core', 'community'] } })
+        .sortBy('position', 'asc')
+        .fetch()
+    })
+    return {
+      teams
+    }
+  }
+})
+</script>
---
+++ nuxtjs.org/components/TeamTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2 class="text-display-4 font-serif">
+    <span class="italic capitalize text-primary-green">{{ slug }}</span> {{ name }}
+  </h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    slug: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
