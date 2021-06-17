066a97c744c60f18a1530c1fe4474e984164575a
Clément Ollivier <clement.o2p@gmail.com>
2021-06-17 10:11:07 +0200

    docs: fix blog posts links and surrounding href

--- nuxtjs.org/components/ContentCardTemplate.vue
+++ nuxtjs.org/components/ContentCardTemplate.vue
@@ -1,23 +1,24 @@
 <template>
   <NuxtLink
-    tag="article"
-    :to="slug"
+    :to="`/blog/${slug}`"
     class="flex flex-col cursor-pointer shadow rounded-lg overflow-hidden dark:bg-secondary-darkest"
   >
-    <div class="aspect-w-16 aspect-h-9 bg-gray-100 dark:bg-secondary-darker mb-4">
-      <NuxtImg :src="image" width="864" height="378" :alt="title" />
-    </div>
-    <div v-if="tags.length" class="px-4 mb-1">
-      <span v-for="(tag, i) in tags" :key="tag" class="text-gray-400 dark:text-cloud font-medium text-sm">
-        <span>{{ tag }}</span>
-        <span v-if="(i === 0 && tags.length > 1) || i !== tags.length - 1"> – </span>
-      </span>
-    </div>
-    <h1 class="font-bold text-body-xl mb-2 px-4">
-      <NuxtLink :to="slug">{{ title }}</NuxtLink>
-    </h1>
-    <p class="pb-8 px-4 text-gray-500 dark:text-cloud-lighter">{{ description }}</p>
-    <slot name="footer" />
+    <article>
+      <div class="aspect-w-16 aspect-h-9 bg-gray-100 dark:bg-secondary-darker mb-4">
+        <NuxtImg :src="image" width="864" height="378" :alt="title" />
+      </div>
+      <div v-if="tags.length" class="px-4 mb-1">
+        <span v-for="(tag, i) in tags" :key="tag" class="text-gray-400 dark:text-cloud font-medium text-sm">
+          <span>{{ tag }}</span>
+          <span v-if="(i === 0 && tags.length > 1) || i !== tags.length - 1"> – </span>
+        </span>
+      </div>
+      <h1 class="font-bold text-body-xl mb-2 px-4">
+        <NuxtLink :to="`/blog/${slug}`">{{ title }}</NuxtLink>
+      </h1>
+      <p class="pb-8 px-4 text-gray-500 dark:text-cloud-lighter">{{ description }}</p>
+      <slot name="footer" />
+    </article>
   </NuxtLink>
 </template>
 
