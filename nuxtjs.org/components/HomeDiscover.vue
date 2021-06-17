8bc9c03ae539ab9f51813dce048c33b7478324f9
Florent <flo@nuxtjs.com>
2021-06-17 17:38:41 +0200

    :sparkles: (HomeDiscover) anim

--- nuxtjs.org/components/HomeDiscover.vue
+++ nuxtjs.org/components/HomeDiscover.vue
@@ -13,9 +13,9 @@
       alt="A landscape image"
     />
 
-    <HomeSection class="py-40 light:bg-gray-50 dark:bg-secondary-darkest">
+    <HomeSection class="pt-40 light:bg-gray-50 dark:bg-secondary-darkest">
       <template #section-content>
-        <SectionContent class="col-span-6 justify-center">
+        <SectionContent class="col-span-12 justify-center">
           <template #category>
             <CategoryLabel label="Discover" />
           </template>
@@ -31,7 +31,7 @@
           </template>
 
           <template #paragraph>
-            <SectionDescription class="pb-8" size="md">
+            <SectionDescription class="py-4 w-full" size="md">
               Nuxt is an Open Source web framework based on official Vue.js libraries, Node.js and using powerful
               development tools such as Vite, Webpack, Babel and PostCSS. Nuxt's purpose is to make web application
               development intuitive and performant with a great developer experience in mind.
@@ -39,17 +39,44 @@
           </template>
         </SectionContent>
       </template>
-
       <template #right-illustration>
-        <div class="col-span-6 w-full flex justify-center items-center md:justify-end">
-          <!-- <button @click="play()">click</button> -->
-          <div id="lottieAnim" ref="lottieAnim" class="sm:w-3/5 md:w-3/4 lg:w-4/6 xl:w-3/5 h-72" />
-          <!-- img
-            class="sm:w-3/5 md:w-3/4 lg:w-4/6 xl:w-3/5"
-            loading="lazy"
-            src="~/static/img/home/discover/modules/discover-modules.svg"
-            alt="Image of a simplified diagram of how Nuxt works"
-          / -->
+        <div class="col-span-12">
+          <div class="flex flex-col md:flex-row justify-center items-center">
+            <div
+              class="
+                grid grid-cols-3
+                gap-8
+                md:gap-0 md:flex md:flex-col
+                items-center
+                md:items-start
+                space-x-4
+                md:space-x-0 md:space-y-2 md:w-1/3
+              "
+            >
+              <div v-for="(animation, index) in animations" :key="animation.name">
+                <div class="flex flex-col-reverse md:flex-row md:flex-row justify-center items-center">
+                  <img
+                    :src="`/img/home/discover/diamond.svg`"
+                    alt="diamond"
+                    class="h-4 w-4 opacity-0"
+                    :class="{ 'opacity-100': index === currentIndex }"
+                  />
+                  <button
+                    class="font-semibold"
+                    :class="
+                      index === currentIndex ? 'text-gray-700 dark:text-white' : 'text-gray-400 dark:text-gray-400'
+                    "
+                    @click="changeAnimation(index)"
+                  >
+                    {{ animation.name }}
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="w-full flex justify-center items-center md:justify-end md:w-2/3">
+              <div id="lottieAnim" ref="lottieAnim" class="h-96" />
+            </div>
+          </div>
         </div>
       </template>
     </HomeSection>
@@ -69,40 +96,85 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref, onMounted } from '@nuxtjs/composition-api'
-import lottie from 'lottie-web'
+import { defineComponent, ref, onMounted, watch } from '@nuxtjs/composition-api'
+import lottie, { AnimationItem } from 'lottie-web'
 
 export default defineComponent({
   setup(_props, context) {
-    const lottieAnim = ref(null)
-    let animation = null
+    const animations = ref([
+      {
+        name: 'Pages',
+        path: 'https://assets3.lottiefiles.com/private_files/lf30_bxmm0fcm.json'
+      },
+      {
+        name: 'UI',
+        path: 'https://assets3.lottiefiles.com/private_files/lf30_rixgn205.json'
+      },
+      {
+        name: 'Data',
+        path: 'https://assets3.lottiefiles.com/private_files/lf30_wtexmd4t.json'
+      },
+      {
+        name: 'Modules',
+        path: 'https://assets3.lottiefiles.com/private_files/lf30_lga5cqha.json'
+      },
+      {
+        name: 'Deployment',
+        path: 'https://assets3.lottiefiles.com/private_files/lf30_farao0hg.json'
+      }
+    ])
 
-    function play() {
-      animation.play()
+    const lottieAnim = ref(null)
+    let anim: AnimationItem
+    const currentIndex = ref(0)
+    const breakLoop = ref(false)
+
+    watch(currentIndex, (newVal, oldVal) => {
+      if (newVal !== oldVal) loadAnimation()
+    })
+
+    function changeAnimation(index: number) {
+      breakLoop.value = true
+      currentIndex.value = index
     }
 
-    onMounted(() => {
-      /**
-       * Use `setTimeout` to ensure slot is mounted and ready to use
-       */
-      setTimeout(() => {
-        animation = lottie.loadAnimation({
-          /**
-           * Temporary use `context.ref` this should replace by Vue3 ref
-           */
-          container: context.refs.lottieAnim as Element,
-          renderer: 'svg',
-          loop: true,
-          autoplay: true,
-          path: 'https://assets3.lottiefiles.com/private_files/lf30_bxmm0fcm.json'
-        })
-      }, 250)
-    })
+    function loadAnimation() {
+      anim?.destroy()
+
+      anim = lottie.loadAnimation({
+        /**
+         * Temporary use `context.ref` this should replace by Vue3 ref
+         */
+        container: context.refs.lottieAnim as Element,
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        path: animations.value[currentIndex.value].path
+      })
+
+      anim.addEventListener('complete', () => {
+        if (!breakLoop.value) {
+          // Set next animation
+          if (currentIndex.value < animations.value.length - 1) {
+            currentIndex.value = currentIndex.value + 1
+          } else {
+            currentIndex.value = 0
+          }
+
+          loadAnimation()
+        }
+      })
+    }
+
+    onMounted(() => setTimeout(loadAnimation, 250))
 
     return {
       lottieAnim,
-      animation,
-      play
+      anim,
+      loadAnimation,
+      changeAnimation,
+      animations,
+      currentIndex
     }
   }
 })
