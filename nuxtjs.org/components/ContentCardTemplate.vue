19e3fdacd2c51c2e9f319362cb3e2db02db7280a
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-06-18 12:02:26 +0200

    :sparkles: (content-card) update content card template w/ UniversalLink

--- nuxtjs.org/components/ContentCardTemplate.vue
+++ nuxtjs.org/components/ContentCardTemplate.vue
@@ -1,23 +1,30 @@
 <template>
   <article class="flex flex-col shadow rounded-lg overflow-hidden dark:bg-secondary-darkest">
     <div class="aspect-h-9 bg-gray-100 dark:bg-secondary-darker mb-4">
-      <NuxtLink :to="`/blog/${slug}`">
+      <UniversalLink :to="href">
         <NuxtImg :src="image" width="864" height="378" :alt="title" />
-      </NuxtLink>
+      </UniversalLink>
     </div>
+
     <div v-if="tags.length" class="px-4 mb-1">
       <span v-for="(tag, i) in tags" :key="tag" class="text-gray-400 dark:text-cloud font-medium text-sm">
         <span>{{ tag }}</span>
         <span v-if="(i === 0 && tags.length > 1) || i !== tags.length - 1"> – </span>
       </span>
     </div>
+
     <div v-else-if="category" class="px-4 mb-1">
       <span class="text-gray-400 dark:text-cloud font-medium text-sm">{{ category }}</span>
     </div>
+
     <h1 class="font-bold text-body-xl mb-2 px-4">
-      <NuxtLink :to="`/blog/${slug}`">{{ title }}</NuxtLink>
+      <UniversalLink :to="href">
+        {{ title }}
+      </UniversalLink>
     </h1>
+
     <p class="pb-8 px-4 text-gray-500 dark:text-cloud-lighter">{{ description }}</p>
+
     <slot name="footer" />
   </article>
 </template>
@@ -39,7 +46,7 @@ export default defineComponent({
       type: String,
       default: ''
     },
-    slug: {
+    href: {
       type: String,
       default: ''
     },
