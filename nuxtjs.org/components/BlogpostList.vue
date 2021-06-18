e2a4ac51ed52b9e455649eee26d4fb249dcbd833
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-06-18 12:02:53 +0200

    :sparkles: (lists) update lists components

--- nuxtjs.org/components/BlogpostList.vue
+++ nuxtjs.org/components/BlogpostList.vue
@@ -8,7 +8,7 @@
         :title="post.title"
         :description="post.description"
         :image="post.imgUrl"
-        :slug="post.slug"
+        :href="`/blog/${post.slug}`"
         :category="post.category"
       >
         <div slot="footer" class="px-4 mb-4 mt-auto">
--- nuxtjs.org/components/ThemesList.vue
+++ nuxtjs.org/components/ThemesList.vue
@@ -2,13 +2,12 @@
   <div class="px-6 mb-8">
     <div class="flex flex-wrap grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       <ContentCardTemplate
-        v-for="({ title, description, image, price, slug }, id) in themes"
+        v-for="({ title, description, image, price }, id) in themes"
         :key="id"
         class=""
         :title="title"
         :description="description"
         :image="image"
-        :slug="slug"
       >
         <div slot="footer" class="px-4 mb-4 mt-auto">
           <SectionButton to="#" size="md" class="bg-primary text-gray-800 hover:bg-green-300 focus:bg-green-300">
