9f22564e667985e95dc10b28ed40a4237203c709
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-06-23 00:35:04 +0200

    :bug: (nuxtjs.org) fix design typography color in dark mode

--- nuxtjs.org/components/organisms/design/DesignTypography.vue
+++ nuxtjs.org/components/organisms/design/DesignTypography.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-sky-darker flex flex-col space-y-4 pt-4">
+  <div class="text-sky-darker dark:text-white flex flex-col space-y-4 pt-4">
     <span class="text-display-4 font-semibold">DM Sans</span>
     <span class="text-display-6 font-semibold"
       >The quick brown fox jumps over<br />
