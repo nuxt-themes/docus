e00be619b25af3b8b41262a8b4447ca719c56b8d
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2021-06-23 01:23:47 +0300

    Tokenisation & Prose components cleanup (#492)
    * ProseA
    
    * ProseBlockquote
    
    * ProseCode
    
    * ProseCodeInline
    
    * :bug: (colors) fix default theme using nuxtjs color
    
    also remove breaky comment :D
    
    Co-authored-by: Yaël GUILLOUX <yael.guilloux@gmail.com>

--- src/defaultTheme/components/atoms/prose/ProseCode.vue
+++ src/defaultTheme/components/atoms/prose/ProseCode.vue
@@ -6,39 +6,10 @@
 </template>
 
 <style lang="postcss" scoped>
-.code-group {
-  pre {
-    @apply rounded-tl-none rounded-tr-none !important;
-  }
-}
 pre {
-  @apply dark:bg-secondary-darkest rounded-lg !important;
-  /* line-height: 1.7142857; */
-  /* border-radius: 0 theme('borderRadius.lg'); */
-  padding: theme('padding.4');
-  margin-top: 0px;
-  margin-bottom: 0px;
-  overflow-x: auto;
-}
-code {
-  /* TODO */
-  /* font-size: 0.875em; */
-  @apply text-sm leading-relaxed;
-  font-weight: 400;
-  color: inherit;
-  background-color: transparent !important;
-  border-radius: theme('borderRadius.md');
-}
-
-pre code {
-  background-color: transparent;
-  font-family: theme('fontFamily.mono');
-}
-
-.dark {
-  pre code {
-    background-color: transparent;
-    color: theme('colors.gray.100');
+  @apply p-4 my-0 overflow-x-auto leading-normal d-prose-code-bg rounded-lg;
+  code {
+    @apply font-normal text-sm leading-relaxed;
   }
 }
 
