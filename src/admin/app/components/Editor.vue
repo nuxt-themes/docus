b16b87b055704704ff6ec910d8be3d8466b34274
Anthony Fu <anthonyfu117@hotmail.com>
2021-06-23 08:06:00 +0800

    feat(admin): merge frontmatter and content

--- src/admin/app/components/Editor.vue
+++ src/admin/app/components/Editor.vue
@@ -1,70 +1,38 @@
 <template>
-  <textarea
-    v-model="frontmatter"
-    class="h-24 w-full font-mono px-4 py-2 d-border-primary border-b outline-none text-sm"
-  />
-  <textarea v-model="content" class="w-full h-full font-mono px-4 py-2 outline-none text-sm" />
+  <textarea v-model="raw" class="w-full h-full font-mono px-4 py-2 outline-none text-sm" />
 </template>
 
-<script lang="ts">
-import YAML from 'js-yaml'
-import { GrayMatterFile } from 'gray-matter'
-import { defineComponent, computed, ref, watch, PropType } from 'vue3'
+<script setup lang="ts">
+import type { PropType } from 'vue3'
+import { ref, watch, defineProps } from 'vue3'
+import type { File } from '../../type'
 import { useApi } from '../plugins/api'
 
-type PermissiveGrayMatterFile = GrayMatterFile<any> & { file: any; path: any }
-
-export default defineComponent({
-  props: {
-    file: {
-      type: Object as PropType<PermissiveGrayMatterFile>,
-      required: true
-    }
-  },
-  setup(props) {
-    const api = useApi()
-
-    // Sync local data when file changes
-    watch(
-      () => props.file,
-      newVal => {
-        data.value = newVal.data
-        content.value = newVal.content
-      }
-    )
-
-    // Local data
-    const data = ref(props.file.data)
-    const content = ref(props.file.content)
-
-    // Stringified reference for frontmatter text-area
-    const frontmatter = computed({
-      get() {
-        return YAML.dump(data.value, null, 2)
-      },
-      set(value: string) {
-        try {
-          data.value = YAML.parse(value)
-        } catch (e) {
-          // New value is not a valid YAML string.
-          // Do nothing and wait for the next valid YAML input.
-        }
-      }
-    })
-
-    // API update on change on data or content
-    watch([data, content], () => {
-      api.put(`/pages${props.file.path}`, {
-        data: data.value,
-        content: content.value
-      })
-    })
-
-    return {
-      frontmatter,
-      content
-    }
+const props = defineProps({
+  file: {
+    type: Object as PropType<File>,
+    required: true
   }
+})
+
+const api = useApi()
+
+// Local data
+const raw = ref(props.file.raw)
+
+// Sync local data when file changes
+watch(
+  () => props.file,
+  newVal => {
+    raw.value = newVal.raw
+  }
+)
+
+// API update on change on data or content
+watch([raw], () => {
+  api.put(`/content${props.file.path}`, {
+    raw: raw.value
+  })
 })
 </script>
 
