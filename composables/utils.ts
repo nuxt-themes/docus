9560ad8d1f949a823e4aeb2abdd6a74f7d93d9d4
Yaël Guilloux <yael.guilloux@gmail.com>
2022-04-26 22:37:40 +0200

    feat(usetheme): drop useTheme composable

--- composables/utils.ts
+++ composables/utils.ts
@@ -1,11 +1,3 @@
-import { useRuntimeConfig } from '#imports'
-
-export const useTheme = () => {
-  const { theme } = useRuntimeConfig()
-
-  return theme
-}
-
 export const classNames = (...args: any[]) => {
   const classes = args.filter(Boolean).join(' ')
   return classes.length ? classes : undefined
