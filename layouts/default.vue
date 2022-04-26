6f5c45f58ab1a92f72197af1f3cfae063d1f5058
Yaël Guilloux <yael.guilloux@gmail.com>
2022-04-26 22:39:44 +0200

    feat(layout): update layouts and use app.vue (maybe fallback to full layouts)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,15 @@
-<script setup>
-import { useContent } from '#imports'
-
-const { page, fetchNavigation } = useContent()
-
-await fetchNavigation()
-</script>
-
 <template>
-  <div class="w-full flex min-h-screen flex-col">
-    <Navbar />
+  <div class="min-h-[calc(100vh-12rem)] sm:min-h-[calc(100vh-8rem)] flex flex-col">
+    <DocsPage>
+      <template #aside>
+        <DocsAside />
+      </template>
 
-    <div class="min-h-[calc(100vh-12rem)] sm:min-h-[calc(100vh-8rem)] flex flex-col">
-      <DocsPage>
-        <template #aside>
-          <DocsAside v-if="page?.value?.aside || typeof page?.value?.aside === 'undefined'" />
-        </template>
-
-        <DocsPageContent>
-          <div class="max-w-none">
-            <NuxtPage />
-          </div>
-        </DocsPageContent>
-      </DocsPage>
-    </div>
-
-    <Footer />
+      <DocsPageContent>
+        <div class="max-w-none">
+          <NuxtPage />
+        </div>
+      </DocsPageContent>
+    </DocsPage>
   </div>
 </template>
--- layouts/page.vue
+++ layouts/page.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="w-full flex min-h-screen flex-col">
-    <Navbar />
-
-    <div class="min-h-[calc(100vh-12rem)] sm:min-h-[calc(100vh-8rem)] flex flex-col ">
-      <NuxtPage />
-    </div>
-
-    <Footer />
+  <div class="min-h-[calc(100vh-12rem)] sm:min-h-[calc(100vh-8rem)] flex flex-col">
+    <NuxtPage />
   </div>
 </template>
