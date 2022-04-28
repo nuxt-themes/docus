be34fca8608d138cf32e5dff981be6568a6e8b31
Yaël Guilloux <yael.guilloux@gmail.com>
2022-04-28 03:09:08 +0200

    feat(composables): add menu & ua composables
    Co-authored-by: Sergey Bedritsky <sergey.bedritsky@gmail.com>

--- composables/useMenu.ts
+++ composables/useMenu.ts
@@ -1,22 +1,7 @@
 import { useState } from '#imports'
 
 export const useMenu = () => {
-  // Menu visible reference
-  const visible = useState('menu-visible', () => false)
+  const { $menu } = useNuxtApp()
 
-  // Open the menu
-  const open = () => (visible.value = true)
-
-  // Close the menu
-  const close = () => (visible.value = false)
-
-  // Toggle the menu (useful for one-off buttons)
-  const toggle = () => (visible.value = !visible.value)
-
-  return {
-    open,
-    close,
-    toggle,
-    visible,
-  }
+  return $menu
 }
---
+++ plugins/user-agent.ts
@@ -0,0 +1,25 @@
+export default defineNuxtPlugin(() => {
+  const isDesktopSafari = ref(false)
+  const isDesktopFirefox = ref(false)
+
+  const refresh = () => {
+    isDesktopSafari.value
+      = !/Mobi|Android/i.test(navigator.userAgent)
+      && /Safari/i.test(navigator.userAgent)
+      && !/Chrome|Chromium/i.test(navigator.userAgent)
+
+    isDesktopFirefox.value = !/Mobi|Android/i.test(navigator.userAgent) && /Firefox/i.test(navigator.userAgent)
+  }
+
+  if (process.client)
+    refresh()
+
+  return {
+    provide: {
+      userAgent: {
+        isDesktopSafari,
+        isDesktopFirefox,
+      },
+    },
+  }
+})
