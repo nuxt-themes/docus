14b61ff2b2c290a6d1d1eddf274188670465c865
Yaël Guilloux <yael.guilloux@gmail.com>
2022-04-29 05:15:50 +0200

    feat(ci): add ci scripts back

---
+++ .github/scripts/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Restore all git changes
+git restore -s@ -SW  -- theme
+
+# Update token
+if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
+  npm whoami
+fi
+
+# Release package
+echo "Publishing Docus..."
+npm publish -q --access public
+echo "Published!"
