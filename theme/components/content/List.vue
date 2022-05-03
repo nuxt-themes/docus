14f9455acaf62335d1b588ad1a8cbbac35873e65
Clément Ollivier <clement.o2p@gmail.com>
2022-05-03 07:05:31 +0200

    fix(theme): add explicit imports for computed

--- theme/components/content/List.vue
+++ theme/components/content/List.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, resolveComponent } from '#imports'
+import { defineComponent, resolveComponent, computed } from '#imports'
 
 const iconTypeMap = {
   primary: 'IconCheck',
