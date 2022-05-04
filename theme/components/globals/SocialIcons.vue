d5783fd85019c8d0bc661286cfa7b82d4d98e4a0
Yaël Guilloux <yael.guilloux@gmail.com>
2022-05-04 23:47:20 +0200

    fix(social-icons): fix social links

--- theme/components/globals/SocialIcons.vue
+++ theme/components/globals/SocialIcons.vue
@@ -16,10 +16,10 @@ const { theme } = useDocus()
 </script>
 
 <template>
-  <a v-if="theme.twitter" :href="`https://twitter.com/${theme.twitter}`" target="_blank" rel="noopener noreferrer" title="Twitter" name="Twitter" class="icon-base" :class="[spacing]">
+  <a v-if="theme.twitter" :href="`https://twitter.com/${theme?.twitter}`" target="_blank" rel="noopener noreferrer" title="Twitter" name="Twitter" class="icon-base" :class="[spacing]">
     <Icon name="fa-brands:twitter" :class="size" />
   </a>
-  <a v-if="theme.github" :href="`https://github.com/${theme.github}`" target="_blank" rel="noopener noreferrer" title="GitHub" name="GitHub" class="icon-base" :class="[spacing]">
+  <a v-if="theme.github?.repository" :href="`https://github.com/${theme?.github?.repository}`" target="_blank" rel="noopener noreferrer" title="GitHub" name="GitHub" class="icon-base" :class="[spacing]">
     <Icon name="fa-brands:github" :class="size" />
   </a>
 </template>
