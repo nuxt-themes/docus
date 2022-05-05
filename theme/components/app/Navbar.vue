0d0808278fda31c8dbca90f430cbcc6b62acc5ac
Sébastien Chopin <seb@nuxtjs.com>
2022-05-05 17:49:01 +0200

    fix: useDocus import

--- theme/components/app/Navbar.vue
+++ theme/components/app/Navbar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from '#imports'
+import { onMounted, onUnmounted, ref, useDocus } from '#imports'
 
 const { theme } = useDocus()
 
@@ -19,17 +19,17 @@ onUnmounted(() => document.removeEventListener('scroll', setOnTop))
 </script>
 
 <template>
-  <header class="surface surface-blurry h-header sticky top-0 z-10 w-full border-b border-gray-200 border-opacity-50 dark:border-gray-800">
-    <Container class="flex h-full flex-none">
-      <div class="flex flex-1 items-center lg:flex-none">
+  <header class="sticky top-0 z-10 w-full border-b border-gray-200 border-opacity-50 surface surface-blurry h-header dark:border-gray-800">
+    <Container class="flex flex-none h-full">
+      <div class="flex items-center flex-1 lg:flex-none">
         <NavbarLogo />
       </div>
 
-      <div class="flex flex-1 items-center">
+      <div class="flex items-center flex-1">
         <NavbarCenter />
       </div>
 
-      <div class="flex flex-1 items-center justify-end lg:gap-4">
+      <div class="flex items-center justify-end flex-1 lg:gap-4">
         <LastRelease v-if="theme?.github?.releases" class="mr-1.5 hidden lg:block" />
         <MobileNav class="flex lg:hidden" />
         <SocialIcons size="h-6 w-6 hidden lg:block" spacing="lg:mr-1.5" />
