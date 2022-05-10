5553597476e0eb7eaf63f4847157e7e24ba73569
Sébastien Chopin <seb@nuxtjs.com>
2022-05-10 15:08:13 +0200

    fix: description is not a shortcut (yet)

--- packages/docs-theme/pages/[...slug].vue
+++ packages/docs-theme/pages/[...slug].vue
@@ -15,8 +15,8 @@ useHead({
   },
   title: page.value?.title,
   titleTemplate: theme.value?.title ? `%s | ${theme.value.title}` : '%s',
-  description: page.value?.description || theme.value?.description || '',
   meta: [
+    { hid: 'description', name: 'description', content: page.value?.description || theme.value?.description },
     { hid: 'og:site_name', property: 'og:site_name', content: 'Nuxt' },
     { hid: 'og:type', property: 'og:type', content: 'website' },
     {
