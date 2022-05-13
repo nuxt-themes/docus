89b484b53d1540dd97e8fae14f72e1d07c9b7287
Yaël Guilloux <yael.guilloux@gmail.com>
2022-05-13 17:01:15 +0200

    chore(deps): upgrade deps

--- packages/github/dist/runtime/composables/githubReleases.d.ts
+++
@@ -1,2 +0,0 @@
-import type { GithubQuery } from '../../module';
-export declare const githubReleases: (query?: Partial<GithubQuery>) => Promise<unknown>;
--- packages/github/dist/runtime/composables/githubReleases.mjs
+++
@@ -1,5 +0,0 @@
-import { withQuery } from "ufo";
-export const githubReleases = (query = {}) => {
-  const url = withQuery("/api/_github/releases", query);
-  return $fetch(url);
-};
