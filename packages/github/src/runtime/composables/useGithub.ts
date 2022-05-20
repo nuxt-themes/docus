92040391a2506239b49cb8e0dfe13fcfd7eee29a
Daniel Roe <daniel@roe.dev>
2022-05-20 15:21:50 +0100

    fix: enforce json return type

--- packages/github/src/runtime/composables/useGithub.ts
+++ packages/github/src/runtime/composables/useGithub.ts
@@ -4,31 +4,31 @@ export const useGithub = () => {
   const fetchRepository = async () => {
     const url = '/api/_github/repository'
 
-    return $fetch(url)
+    return $fetch(url, { responseType: 'json' })
   }
 
   const fetchReleases = async (query: Partial<any>) => {
     const url = withQuery('/api/_github/releases', query as any)
 
-    return $fetch(url)
+    return $fetch(url, { responseType: 'json' })
   }
 
   const fetchLastRelease = async (query: Partial<any>) => {
     const url = withQuery('/api/_github/releases', { ...query, last: true } as any)
 
-    return $fetch(url)
+    return $fetch(url, { responseType: 'json' })
   }
 
   const fetchContributors = async (query: any) => {
     const url = withQuery('/api/_github/contributors', query as any)
 
-    return $fetch(url)
+    return $fetch(url, { responseType: 'json' })
   }
 
   const fetchFileContributors = async (source = '', query: any) => {
     const url = withQuery(`/api/_github/contributors/file`, { ...query, source } as any)
 
-    return $fetch(url)
+    return $fetch(url, { responseType: 'json' })
   }
 
   return {
