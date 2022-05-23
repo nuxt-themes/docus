3787df57070eb190bed4165f038862f7263e4737
Serhii <bedrytskyi@gmail.com>
2022-05-23 13:16:26 +0300

    add bg to scrim overlay

--- packages/docs-theme/components/app/MobileNav.vue
+++ packages/docs-theme/components/app/MobileNav.vue
@@ -21,8 +21,17 @@ const buttonClasses = 'w-8 h-8 icon-base rounded-xl'
 </script>
 
 <template>
-  <button :class="[buttonClasses]" class="relative z-10 lg:hidden" aria-label="Menu" @click="toggle">
-    <Icon name="heroicons-outline:menu" class="w-6 h-6 mx-auto icon-base" aria-hidden="”true”" />
+  <button
+    :class="[buttonClasses]"
+    class="relative z-10 lg:hidden"
+    aria-label="Menu"
+    @click="toggle"
+  >
+    <Icon
+      name="heroicons-outline:menu"
+      class="icon-base mx-auto h-6 w-6"
+      aria-hidden="”true”"
+    />
   </button>
 
   <span class="lg:hidden">
@@ -31,8 +40,12 @@ const buttonClasses = 'w-8 h-8 icon-base rounded-xl'
         <!-- Scrim overlay -->
         <div
           id="mobile-nav-scrim"
-          :class="[visible ? 'pointer-events-auto opacity-100' : 'pointer-events-none opacity-0']"
-          class="fixed top-0 left-0 z-10 w-full h-full transition bg-opacity-50 backdrop-blur dark:bg-opacity-50"
+          :class="[
+            visible
+              ? 'pointer-events-auto opacity-100'
+              : 'pointer-events-none opacity-0',
+          ]"
+          class="fixed top-0 left-0 z-10 h-full w-full bg-white bg-opacity-50 backdrop-blur transition dark:bg-opacity-50"
           @click="toggle"
         />
 
@@ -41,25 +54,38 @@ const buttonClasses = 'w-8 h-8 icon-base rounded-xl'
           id="mobile-nav-button"
           :class="[
             buttonClasses,
-            visible ? 'pointer-events-auto opacity-100' : `opacity-0 transition ${isDesktopSafari || isDesktopFirefox ? 'duration-0' : 'duration-400'} pointer-events-none`,
+            visible
+              ? 'pointer-events-auto opacity-100'
+              : `opacity-0 transition ${
+                  isDesktopSafari || isDesktopFirefox
+                    ? 'duration-0'
+                    : 'duration-400'
+                } pointer-events-none`,
           ]"
           class="fixed z-30"
           @click="toggle"
         >
-          <Icon name="heroicons-outline:x" class="w-6 h-6 mx-auto icon-base" />
+          <Icon name="heroicons-outline:x" class="icon-base mx-auto h-6 w-6" />
         </button>
 
         <!-- Nav menu surface -->
         <div
           id="mobile-nav-surface"
-          :class="[visible ? 'pointer-events-auto scale-100 opacity-100' : 'pointer-events-none scale-95 opacity-0']"
-          class="fixed z-20 w-[calc(100%-4rem)] min-w-full origin-top-right overflow-hidden transform transition-transform ease-out sm:w-auto sm:min-w-[calc(320px-2rem)] border shadow-xl surface surface-border rounded-2xl"
+          :class="[
+            visible
+              ? 'pointer-events-auto scale-100 opacity-100'
+              : 'pointer-events-none scale-95 opacity-0',
+          ]"
+          class="surface surface-border fixed z-20 w-[calc(100%-4rem)] min-w-full origin-top-right transform overflow-hidden rounded-2xl border shadow-xl transition-transform ease-out sm:w-auto sm:min-w-[calc(320px-2rem)]"
           @click="toggle"
         >
-          <div class="z-40 max-h-full pt-12 pb-6 pl-8 pr-0 mb-2 overflow-y-auto custom-scrollbar" @click.stop.prevent>
+          <div
+            class="custom-scrollbar z-40 mb-2 max-h-full overflow-y-auto pt-12 pb-6 pl-8 pr-0"
+            @click.stop.prevent
+          >
             <DocsAsideTree :links="links" />
 
-            <div class="flex items-center justify-end gap-4 px-6 mt-4">
+            <div class="mt-4 flex items-center justify-end gap-4 px-6">
               <SocialIcons />
               <ThemeSelect size="h-6 w-6" />
             </div>
