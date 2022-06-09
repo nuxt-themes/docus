59bf163250cdeb5c61935d758ba3363fa4404ed6
Benjamin Canac <canacb1@gmail.com>
2022-06-09 15:24:02 +0200

    up

--- packages/docs-theme/components/app/PoweredByDocus.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="u-border-gray-200 border-t border-dashed p-3 text-center">
-    <a href="https://docus.dev" target="_blank" class="u-text-gray-400 hover:u-text-gray-600 group text-xs"> Powered by <strong class="font-bold">Docus</strong> </a>
-  </div>
-</template>
