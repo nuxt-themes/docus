693d84f8cf4d36d297496ed96bab92a1fa83a6f4
Yaël Guilloux <yael.guilloux@gmail.com>
2022-06-13 17:22:19 +0200

    feat(github): improve github module implementation (#578)

--- packages/github/src/runtime/components/GithubReleases.ts
+++ packages/github/src/runtime/components/GithubReleases.ts
@@ -2,7 +2,7 @@ import { defineComponent, ref, useSlots } from 'vue'
 import type { PropType } from 'vue'
 import { useGithub } from '../composables/useGithub'
 import type { GithubReleasesQuery } from '../../module'
-import { useAsyncData, useState } from '#imports'
+import { useAsyncData } from '#imports'
 
 export default defineComponent({
   props: {
@@ -14,11 +14,7 @@ export default defineComponent({
   async setup(props) {
     const { fetchReleases } = useGithub()
 
-    const { data: _releases, refresh } = await useAsyncData('github-releases-component', () => fetchReleases(props.query))
-
-    // TODO: remove this painful workaround: hotfix for https://github.com/vuejs/core/issues/5513
-    const releases = process.client ? useState('github-releases') : ref()
-    releases.value = releases.value || _releases.value
+    const { data: releases, refresh } = await useAsyncData('github-releases-component', () => fetchReleases(props.query))
 
     return {
       releases,
