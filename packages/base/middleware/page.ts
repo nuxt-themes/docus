1138f930b3ce1a30cb8054b784a0741f3f296600
Yaël Guilloux <yael.guilloux@gmail.com>
2022-06-16 13:07:34 +0200

    chore(cleanup): cleanup page middleware

--- packages/base/middleware/page.ts
+++ packages/base/middleware/page.ts
@@ -2,14 +2,10 @@ import { queryPage } from '../utils/queries'
 import { defineNuxtRouteMiddleware, useNuxtApp } from '#imports'
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  if (to.path === '/favicon.icon') return
-
   if (process.client && to.path !== from.path) {
     // @ts-expect-error - Untyped hook
     useNuxtApp().callHook('docus:page:start')
   }
-
-  if (to === 'favicon.icon') return
 
   const data = await queryPage(to)
 
