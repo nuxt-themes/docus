6c47455108d1a2786cd556f0d6771382594b18f6
Benjamin Canac <canacb1@gmail.com>
2022-06-24 15:12:05 +0200

    chore(docs-theme): improve search button design

--- packages/docs-theme/components/app/Search.vue
+++ packages/docs-theme/components/app/Search.vue
@@ -10,11 +10,11 @@ const onClick = () => element.value.querySelector('button').click()
   <div class="relative pointer-events-auto group" @click="onClick">
     <button
       type="button"
-      class="u-text-gray-500 hover:u-text-gray-700 transition-colors duration-300 w-full flex items-center text-sm leading-6 text-slate-400 rounded-md ring-1 dark:ring-gray-900 ring-gray-100 group-hover:ring-gray-500 hover:ring-gray-500 shadow-sm py-1.5 pl-2 pr-3"
+      class="u-text-gray-500 hover:u-text-gray-700 duration-300 w-full flex items-center text-sm leading-6 rounded-md ring-1 u-ring-gray-100 group-hover:u-ring-gray-300 shadow-sm py-1.5 pl-2 pr-3"
     >
-      <Icon name="heroicons-outline:search" class="mr-3 flex-none" />
-      <span class="text-xs font-semibold">Search</span>
-      <span class="ml-auto pl-3 flex-none text-xs font-semibold hidden md:block">⌘K</span>
+      <Icon name="heroicons-outline:search" class="mr-2 flex-none w-4 h-4" />
+      <span class="text-xs font-medium">Search</span>
+      <span class="ml-auto pl-3 flex-none text-xs font-semibold hidden md:block u-text-gray-400">⌘K</span>
     </button>
   </div>
 </template>
