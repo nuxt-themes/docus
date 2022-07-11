21006eadc6e8bd97545b97fe466038f75a4cb04f
Yaël GUILLOUX <yael.guilloux@gmail.com>
2022-07-11 18:57:49 +0200

    ci(scripts): update release scripts

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# Resolve yarn
+YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
+
 # Update token
 if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
   echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
   echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
   echo "always-auth=true" >> ~/.npmrc
+  echo "npmAuthToken: ${NODE_AUTH_TOKEN}" >> ~/.yarnrc.yml
   npm whoami
 fi
 
 # Release package
 echo "Publishing $PWD"
-# npm publish -q --access public
+# yarn npm publish --access public --tolerate-republish
 echo "Published!"
