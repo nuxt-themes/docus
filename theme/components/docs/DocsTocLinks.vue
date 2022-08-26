fd07a18a1ef6f9601055d8caec3b45b4ed85c409
Ahad Birang <farnabaz@gmail.com>
2022-08-26 12:26:24 +0200

    fix(TOC): active headings query

--- theme/components/docs/DocsTocLinks.vue
+++ theme/components/docs/DocsTocLinks.vue
@@ -22,10 +22,10 @@ watch(
     if (process.client) {
       setTimeout(() => {
         updateHeadings([
-          ...document.querySelectorAll('.docus-content h1'),
-          ...document.querySelectorAll('.docus-content h2'),
-          ...document.querySelectorAll('.docus-content h3'),
-          ...document.querySelectorAll('.docus-content h4')
+          ...document.querySelectorAll('.document-driven-page h1'),
+          ...document.querySelectorAll('.document-driven-page h2'),
+          ...document.querySelectorAll('.document-driven-page h3'),
+          ...document.querySelectorAll('.document-driven-page h4')
         ])
       }, 300)
     }
@@ -43,6 +43,7 @@ const scrollToHeading = (id: string) => {
 
 <template>
   <ul>
+    {{ activeHeadings }}
     <li v-for="link in links" :key="link.text" class="min-w-0 group" :class="[{ 'pl-3': link.depth === 3, 'pl-6': link.depth === 4 }]">
       <a
         :href="`#${link.id}`"
