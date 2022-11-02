17de2a24f9f81f248f2336a012d0a96b0b7a06d1
Sébastien Chopin <seb@nuxtjs.com>
2022-11-02 19:28:49 +0100

    chore: add playground

---
+++ .playground/components/content/CounterButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+const count = ref(0)
+</script>
+
+<template>
+  <button @click="count++">
+    {{ count }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  border: 1px #ddd solid;
+  padding: 3px 10px;
+  border-radius: 5px;
+}
+</style>
