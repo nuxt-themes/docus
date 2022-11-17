74fe2ab276a2e9f60f0768bbfd064aa380947d0f
Yaël Guilloux <yael.guilloux@gmail.com>
2022-11-17 17:38:24 +0100

    chore(format): remove leading linebreak

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,7 +1,6 @@
 import type { RouterConfig } from '@nuxt/schema'
 // https://router.vuejs.org/api/interfaces/routeroptions.html
 export default <RouterConfig> {
-
   scrollBehavior (to, from, savedPosition) {
     if (to.hash) {
       const { marginTop } = getComputedStyle((document.querySelector(to.hash) as any))
