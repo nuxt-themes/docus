3f4595c880f6e2e253df2e3da8b980bd3b835375
Yaël GUILLOUX <yael.guilloux@gmail.com>
2023-01-17 14:56:43 +0100

    chore(deps): upgrade deps ; upgrade schema

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -24,6 +24,7 @@ export default defineNuxtConfigSchema({
        * @example '/cover.jpg'
        *
        * @studio-icon dashicons:cover-image
+       * @studio-input file
        */
       image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
       /**
@@ -128,6 +129,7 @@ export default defineNuxtConfigSchema({
          * String to be used as a name of a component.
          *
          * @example 'MyLogo'
+         * @studio-input component
          */
         logo: false,
         /**
