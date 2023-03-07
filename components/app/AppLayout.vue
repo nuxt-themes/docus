cb9d4b8d7a89e0d91fea93fe085cece5bd8fae69
Serhii Bedrytskyi <bedrytskyi@gmail.com>
2023-03-07 13:12:12 +0200

    feat: sticky footer (#840)

--- components/app/AppLayout.vue
+++ components/app/AppLayout.vue
@@ -17,10 +17,20 @@ useContentHead(config.value as any)
 </script>
 
 <template>
-  <div>
+  <div class="app-layout">
     <AppLoadingBar />
     <AppHeader />
     <slot />
     <AppFooter />
   </div>
 </template>
+
+<style scoped lang="ts">
+css({
+  '.app-layout': {
+    minHeight: '100vh',
+    display: 'grid',
+    gridTemplateRows: 'auto 1fr auto',
+  }
+})
+</style>
