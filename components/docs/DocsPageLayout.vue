8b920c2b6e2ba2d36230c9a020cebed90e16a09b
Sébastien Chopin <seb@nuxt.com>
2023-03-08 20:26:45 +0100

    fix: show aside when level 0 and one parent only

--- components/docs/DocsPageLayout.vue
+++ components/docs/DocsPageLayout.vue
@@ -11,7 +11,7 @@ const fallbackValue = (value: string, fallback = true) => {
 const hasBody = computed(() => !page.value || page.value?.body?.children?.length > 0)
 const hasToc = computed(() => page.value?.toc !== false && page.value?.body?.toc?.links?.length >= 2)
 
-const hasAside = computed(() => page.value?.aside !== false && tree.value?.length > 1)
+const hasAside = computed(() => page.value?.aside !== false && (tree.value?.length > 1 || tree.value?.[0]?.children?.length))
 const bottom = computed(() => fallbackValue('bottom', true))
 const isOpen = ref(false)
 
