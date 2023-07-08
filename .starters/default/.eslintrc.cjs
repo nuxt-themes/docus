aeef2d8f899b7737d2680ed03d08743cdd7b3090
Atle Stromsvold <obsfeil@outlook.com>
2023-07-08 22:12:09 +0200

    l,,

--- .starters/default/.eslintrc.cjs
+++ .starters/default/.eslintrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  root: true,
-  extends: '@nuxt/eslint-config',
-  rules: {
-    'vue/max-attributes-per-line': 'off',
-    'vue/multi-word-component-names': 'off'
-  }
+    root: true,
+    extends: '@nuxt/eslint-config',
+    rules: {
+        'vue/max-attributes-per-line': 'off',
+        'vue/multi-word-component-names': 'on'
+    }
 }
--- layouts/page.vue
+++ layouts/page.vue
@@ -1,9 +1,15 @@
-                    <script setup lang="ts">const {
-                    config
-                }
-                
-                =useDocus() </script><template><div class="page-layout"><Container :fluid="config?.main?.fluid" :padded="config?.main?.padded"><article><slot /></article></Container></div></template><style lang="ts" scoped>css( {
-                    '.page-layout': {
-                        display: 'flex', flexDirection: 'column', position: 'relative'
-                    }
-                    , ) </style>
+<script setup lang="ts">const {
+    page
+}
+
+=useContent() const {
+    layoutConfig
+}
+
+=useCurrentNavigation() </script><template><div class="page-layout"><Container :fluid="page?.fluid || layoutConfig?.fluid" :padded="page?.padded || layoutConfig?.padded || true"><article><slot /></article></Container></div></template><style lang="ts" scoped>css( {
+    '.page-layout': {
+        display: 'flex', flexDirection: 'column', position: 'relative'
+    }
+}
+
+) </style>
