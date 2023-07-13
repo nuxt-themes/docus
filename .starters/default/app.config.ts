119fec6dc490e6d544b94895bac9576d348c228d
Atle Stromsvold <obsfeil@outlook.com>
2023-07-14 00:05:04 +0200

    commit 3

--- .starters/default/app.config.ts
+++ .starters/default/app.config.ts
@@ -36,3 +36,4 @@ export default defineAppConfig({
     }
   }
 })
+
--- .starters/default/nuxt.config.ts
+++ .starters/default/nuxt.config.ts
@@ -6,6 +6,21 @@ export default defineNuxtConfig({
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible',
     // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
-  ]
+    '@nuxt/devtools',
+    'nuxt-icon',
+    '@nuxt/content'
+  ],
+
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+    markdown: {
+      tags: {
+        p: ''
+      },
+    },
+  },
+
+  devtools: {
+    enabled: true
+  }
 })
