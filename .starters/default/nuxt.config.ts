e15364a00169d773cbd22aa82b55867f685a819f
Sébastien Chopin <seb@nuxtjs.com>
2023-08-03 13:31:58 +0200

    chore: remove extra runtimeconfig

--- .starters/default/nuxt.config.ts
+++ .starters/default/nuxt.config.ts
@@ -7,17 +7,5 @@ export default defineNuxtConfig({
     '@nuxtjs/plausible',
     // https://github.com/nuxt/devtools
     '@nuxt/devtools'
-  ],
-  runtimeConfig: {
-    public: {
-      algolia: {
-        applicationId: 'Q46Q8609QS',
-        apiKey: '1661c72375532f074aedd9452a1270be',
-        langAttribute: 'lang',
-        docSearch: {
-          indexName: 'content-nuxtjs'
-        }
-      }
-    }
-  }
+  ]
 })
