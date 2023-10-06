8f0dcffe3eb73e52f6c97e1d36f47ecff08ec39a
Serhii Bedrytskyi <bedrytskyi@gmail.com>
2023-10-06 14:46:53 +0200

    feat: transfer AppFooter tokens

--- components/app/AppFooter.vue
+++ components/app/AppFooter.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import { useElementBounding } from '@vueuse/core'
-import appConfig from '#build/app.config'
 
 const appFooterRef = ref(null) as Ref<HTMLElement | null>
 
 const { height } = useElementBounding(appFooterRef)
-const { tokens } = appConfig
 const { config } = useDocus()
 const socialIcons = ref(null)
 const icons = computed(() => config.value?.footer?.iconLinks || [])
@@ -21,30 +19,24 @@ watch(height, (value) => {
 <template>
   <footer
     ref="appFooterRef"
-    :class="[Object.values(tokens.appFooter.root)]"
+    class="flex min-h-[145px] sm:min-h-[100px] border-t border-primary-static py-4"
   >
     <Container
       padded
-      class="footer-layout"
-      :class="[Object.values(tokens.appFooter.layout.root)]"
+      class="footer-layout grid grid-cols-12 justify-items-center sm:justify-items-stretch gap-2"
     >
-      <div
-        class="left"
-        :class="[Object.values(tokens.appFooter.layout.left)]"
-      >
+      <div class="left flex col-span-12 sm:col-span-3 order-1 sm:order-none py-4">
         <a
           v-if="config?.footer?.credits"
           :href="config?.footer?.credits?.href || '#'"
           rel="noopener"
           target="_blank"
-          class="footer-credits flex items-center"
-          :class="[Object.values(tokens.appFooter.credits.text)]"
+          class="footer-credits flex items-center text-xs font-medium text-secondary-static text-secondary-hover"
         >
           <Component
             :is="config?.footer?.credits?.icon"
             v-if="config?.footer?.credits?.icon"
-            class="credits-icon"
-            :class="[Object.values(tokens.appFooter.credits.icon)]"
+            class="credits-icon w-4 h-4 me-2 shrink-0 text-current"
           />
           <span
             v-if="config?.footer?.credits?.text"
@@ -54,15 +46,11 @@ watch(height, (value) => {
         </a>
       </div>
 
-      <div
-        class="center"
-        :class="[Object.values(tokens.appFooter.layout.center)]"
-      >
+      <div class="center flex flex-col sm:flex-row items-center justify-start sm:justify-center col-span-12 sm:col-span-6">
         <NuxtLink
           v-for="link in textLinks"
           :key="link.href"
-          class="text-link"
-          :class="[Object.values(tokens.appFooter.textLink)]"
+          class="text-link p-2 text-sm font-medium"
           :aria-label="link.text"
           :href="link.href"
           :target="link?.target || '_self'"
@@ -72,15 +60,11 @@ watch(height, (value) => {
         </NuxtLink>
       </div>
 
-      <div
-        class="right"
-        :class="[Object.values(tokens.appFooter.layout.right)]"
-      >
+      <div class="right flex items-center justify-end col-span-12 sm:col-span-3 sm:-me-4">
         <a
           v-for="icon in icons.slice(0, 6 - nbSocialIcons)"
           :key="icon.label"
-          class="icon-link"
-          :class="[Object.values(tokens.appFooter.iconLink)]"
+          class="icon-link [&_.icon]:w-4 [&_.icon]:h-4 flex p-4 text-secondary-static text-secondary-hover"
           :aria-label="icon.label"
           :href="icon.href"
           target="_blank"
@@ -90,7 +74,7 @@ watch(height, (value) => {
         </a>
         <AppSocialIcons
           ref="socialIcons"
-          :class="[tokens.appFooter.iconLink.width, tokens.appFooter.iconLink.height]"
+          class="[&_.icon]:w-4 [&_.icon]:h-4"
         />
       </div>
     </Container>
