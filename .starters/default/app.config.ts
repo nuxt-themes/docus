cdfa94083be3bbc973115c5e9173c150fd253f7b
Shimpei Iwamaru <gangan.education@gmail.com>
2024-03-05 23:20:34 +0900

    chore(starter): update extends

--- .starters/default/app.config.ts
+++ .starters/default/app.config.ts
@@ -1,3 +1,4 @@
+// https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
     title: 'Docus',
--- .starters/default/nuxt.config.ts
+++ .starters/default/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus',
+  extends: ['@nuxt-themes/docus'],
   devtools: { enabled: true },
 
   modules: [
