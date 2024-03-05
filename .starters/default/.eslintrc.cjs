3bf310173fbf75e68bd15fad9d706332cd04818c
gangan <44604921+shinGangan@users.noreply.github.com>
2024-03-06 03:37:59 +0900

    chore: update starter deps (#1021)

--- .starters/default/.eslintrc.cjs
+++ .starters/default/.eslintrc.cjs
@@ -1,6 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@nuxt/eslint-config',
+  extends: ['@nuxt/eslint-config'],
+  ignorePatterns: [
+    'dist',
+    'node_modules',
+    '.output',
+    '.nuxt'
+  ],
   rules: {
     'vue/max-attributes-per-line': 'off',
     'vue/multi-word-component-names': 'off'
